e6b52a43e9adf6836aac1fb1380688fcdad3705d
Björn Harrtell <bjorn@wololo.org>
2011-02-14 09:48:47 +0100

    Added more testcases for Envelope and fix implementation errors that was found.

--- test/spec/jsts/geom/Envelope.js
+++ test/spec/jsts/geom/Envelope.js
@@ -1,16 +1,30 @@
 describe('jsts.geom.Envelope', function() {
-  var e1, e2, e3, e4;
-  
+  var e1, e2, e3, e4, e5;
+
   it('can be constructed', function() {
-    e1 = new jsts.geom.Envelope(1, 2, 3, 4);
-    e2 = new jsts.geom.Envelope(new jsts.geom.Coordinate(1, 3), new jsts.geom.Coordinate(2, 4));
-    e3 = new jsts.geom.Envelope(new jsts.geom.Coordinate(1, 2));
+    e1 = new jsts.geom.Envelope(1, 4, 1, 4);
+    e2 = new jsts.geom.Envelope(new jsts.geom.Coordinate(1, 1), new jsts.geom.Coordinate(4, 4));
+    e3 = new jsts.geom.Envelope(new jsts.geom.Coordinate(1, 4));
     e4 = new jsts.geom.Envelope(e1);
+    e5 = new jsts.geom.Envelope(2, 8, 2, 8);
     expect(e1).toBeDefined();
   });
-  
+
   it('should be equal to another Envelope defined from the same coordinates', function() {
     expect(e1.equals(e2)).toEqual(true);
   });
-  
+
+  it('can be expanded to inlude another Envelope', function() {
+    var clone = new jsts.geom.Envelope(e1);
+    clone.expandToInclude(e5);
+    var expectedEnvelope = new jsts.geom.Envelope(1, 8, 1, 8);
+    expect(clone.equals(expectedEnvelope)).toEqual(true);
+  });
+
+  it('can calculate its intersection with another Envelope', function() {
+    var intersectingEnvelope = e1.intersection(e5);
+    var expectedEnvelope = new jsts.geom.Envelope(2, 4, 2, 4);
+    expect(intersectingEnvelope.equals(expectedEnvelope)).toEqual(true);
+  });
+
 });
