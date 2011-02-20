7558eaba70b82160dbda6c1939a1dc4f0b156bd3
Björn Harrtell <bjorn@wololo.org>
2011-02-20 19:31:51 +0100

    Added next set of classes that will have to be ported before anything real can be done

---
+++ test/spec/jsts/geom/Geometry.js
@@ -0,0 +1,9 @@
+/* Copyright (c) 2011 by Björn Harrtell.
+ * Published under the GNU Lesser GPL 2.1 license.
+ * See https://github.com/bjornharrtell/jsts/blob/master/license.txt for the
+ * full text of the license.
+ */
+
+describe('jsts.geom.Geometry', function() {
+ 
+});
