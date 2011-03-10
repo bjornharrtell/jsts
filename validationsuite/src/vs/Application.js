d90b0f77f67acdc898e7fc97a119b4ee2a295ccc
Björn Harrtell <bjorn@wololo.org>
2011-03-10 15:15:49 +0100

    Some skeleton application code for the validation suite

--- validationsuite/src/vs/Application.js
+++ validationsuite/src/vs/Application.js
@@ -1,1 +1,37 @@
-// TODO: use GeoExt, parse test data from testxml 
+// TODO: use GeoExt, parse test data from testxml
+
+jsts.vs.Application = {
+  init: function() {
+    new Ext.Viewport({
+      layout: 'absolute',
+      items: [{
+        title: 'Choose test case',
+        x: 20,
+        y: 20,
+        height: 100,
+        width: 600,
+        xtype: 'form',
+        frame: true,
+        html: 'TODO: Replace with form controls to choose test case'
+      }, {
+        title: 'Test case details',
+        x: 20,
+        y: 130,
+        height: 200,
+        width: 600,
+        frame: true,
+        html: 'TODO: Replace with test case details'
+      }, {
+        title: 'Test case results',
+        x: 20,
+        y: 340,
+        height: 200,
+        width: 600,
+        frame: true,
+        html: 'TODO: Replace with test case results'
+      }]
+    });
+  }
+};
+
+Ext.onReady(jsts.vs.Application.init);
