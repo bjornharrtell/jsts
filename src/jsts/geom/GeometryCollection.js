a7e4723cc97b361234b33e2143576ac807a2d4ca
Björn Harrtell <bjorn@wololo.org>
2011-03-14 17:34:34 +0100

    Switch back to LGPL 2.1

--- src/jsts/geom/GeometryCollection.js
+++ src/jsts/geom/GeometryCollection.js
@@ -1,3 +1,11 @@
+/* Copyright (c) 2011 by Björn Harrtell.
+ * Published under the LGPL 2.1 license.
+ * See /license-notice.txt for the full text of the license notice.
+ * See /license.txt for the full text of the license.
+ */
+
+
+
 /**
  * @constructor
  * @extends {OpenLayers.Geometry.Collection}
