03b05be3fe6d54e37beff353743eb4882cf2d479
johanhasselstrom <johan@hasselstrom.se>
2011-03-17 05:08:01 +0800

    Fixed jslin errors in IntervalSize.

--- src/jsts/index/IntervalSize.js
+++ src/jsts/index/IntervalSize.js
@@ -1,13 +1,15 @@
 
+
 /**
  * Provides a test for whether an interval is so small it should be considered
  * as zero for the purposes of inserting it into a binary tree. The reason this
  * check is necessary is that round-off error can cause the algorithm used to
  * subdivide an interval to fail, by computing a midpoint value which does not
  * lie strictly between the endpoints.
- * 
+ *
  */
 jsts.index.IntervalSize = OpenLayers.Class();
+
 
 /**
  * This value is chosen to be a few powers of 2 less than the number of bits
@@ -17,21 +19,23 @@ jsts.index.IntervalSize = OpenLayers.Class();
  */
 jsts.index.IntervalSize.prototype.MIN_BINARY_EXPONENT = -50;
 
+
 /**
  * Computes whether the interval [min, max] is effectively zero width. I.e. the
  * width of the interval is so much less than the location of the interval that
  * the midpoint of the interval cannot be represented precisely.
- * 
+ *
  * @param {Number}
- *          min the min-value in the interval
+ *          min the min-value in the interval.
  * @param {Number}
- *          max the max-value in the interval
+ *          max the max-value in the interval.
  * @return {Boolean} true if the interval should be considered zero.
  */
 jsts.index.IntervalSize.prototype.isZeroWidth = function(min, max) {
   var width = max - min;
-  if (width == 0.0)
+  if (width === 0.0) {
     return true;
+  }
 
   var maxAbs, scaledInterval, level;
   maxAbs = Math.max(Math.abs(min), Math.abs(max));
