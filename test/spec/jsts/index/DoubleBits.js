1169f4e86efeb43f457ea38bfa2af5c637fc432c
johanhasselstrom <johan@hasselstrom.se>
2011-03-17 04:46:46 +0800

    Fixed error in exponent calculation. Added the IntervalSize-class along with test-case.

--- test/spec/jsts/index/DoubleBits.js
+++ test/spec/jsts/index/DoubleBits.js
@@ -1,8 +1,8 @@
 describe('jsts.index.DoubleBits', function() {
   it('calculates correct exponent', function() {
     var db = jsts.index.DoubleBits.prototype;
-    expect(db.exponent(-1)===0);
-    expect(db.exponent(8.0)===3);
-    expect(db.exponent(128.0) === 7);
+    expect(db.exponent(-1)).toBe(0);
+    expect(db.exponent(8.0)).toBe(3);
+    expect(db.exponent(128.0)).toBe(7);
   });
 });
