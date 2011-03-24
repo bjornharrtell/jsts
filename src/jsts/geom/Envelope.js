81db2b240e09e34d34882d162dde914e0b826986
johanhasselstrom <johan@hasselstrom.se>
2011-03-24 21:24:53 +0100

    Implemented KdTree and KdNode

--- src/jsts/geom/Envelope.js
+++ src/jsts/geom/Envelope.js
@@ -494,9 +494,8 @@ jsts.geom.Envelope.prototype.intersectsEnvelope = function(other) {
   if (this.isNull() || other.isNull()) {
     return false;
   }
-  var result = !(other.minx > this.maxx || other.maxx < this.minx ||
-      other.miny > this.maxy || other.maxy < this.miny); 
-  return result;
+  return !(other.minx > this.maxx || other.maxx < this.minx ||
+      other.miny > this.maxy || other.maxy < this.miny);
 };
 
 
