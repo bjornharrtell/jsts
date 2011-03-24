96b228b81637ea5242881eb20535b4e381b4c08b
Johan Hasselström <johan@hasselstrom.se>
2011-03-24 16:17:40 +0100

    Added copyright notice

--- src/jsts/index/quadtree/Root.js
+++ src/jsts/index/quadtree/Root.js
@@ -1,4 +1,8 @@
-
+/* Copyright (c) 2011 by The Authors.
+ * Published under the LGPL 2.1 license.
+ * See /license-notice.txt for the full text of the license notice.
+ * See /license.txt for the full text of the license.
+*/
 
 /**
  * QuadRoot is the root of a single Quadtree. It is centred at the origin, and
