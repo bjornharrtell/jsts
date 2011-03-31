186db27fde91860b7c66d75c07a6f40b4f9b7fe7
Björn Harrtell <bjorn@wololo.org>
2011-03-31 21:12:48 +0200

    Implement getFactory for geometries Cleaned up error classes

--- src/jsts.js
+++ src/jsts.js
@@ -11,7 +11,8 @@
  * @namespace
  */
 jsts = {
-  version: '1.11-SNAPSHOT'
+  version: '1.11-SNAPSHOT',
+  error: {}
 };
 
 
@@ -21,38 +22,19 @@ jsts = {
  *          message User defined error message.
  * @constructor
  */
-jsts.IllegalArgumentError = function(message) {
-};
-jsts.IllegalArgumentError = OpenLayers.Class(Error);
-
-
-
-/**
- * Overridden constructor
- * @param {string} message error message.
- * @constructor
- */
-jsts.IllegalArgumentError.initialize = function(message) {
+jsts.error.IllegalArgumentError = function(message) {
   this.name = 'IllegalArgumentError';
   this.message = (message || '');
 };
+jsts.error.IllegalArgumentError.prototype = new Error();
 
 
 
 /**
  * @constructor
  */
-jsts.AbstractMethodInvocationError = function() {
-};
-jsts.AbstractMethodInvocationError = OpenLayers.Class(Error);
-
-
-
-/**
- * Overridden constructor
- * @constructor
- */
-jsts.AbstractMethodInvocationError.initialize = function() {
+jsts.error.AbstractMethodInvocationError = function() {
   this.name = 'AbstractMethodInvocationError';
   this.message = 'Abstract method called, should be implemented in subclass.';
 };
+jsts.error.AbstractMethodInvocationError.prototype = new Error();
