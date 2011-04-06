64184681e99a81469e6088b809cfa950d2c13242
johanhasselstrom <johan@hasselstrom.se>
2011-04-06 20:51:25 +0200

    Changed behaviour in tests since static functions no longer are in the prototype object

--- test/spec/jsts/index/IntervalSize.js
+++ test/spec/jsts/index/IntervalSize.js
@@ -1,5 +1,5 @@
 describe('jsts.index.IntervalSize', function() {
-  var is = jsts.index.IntervalSize.prototype;
+  var is = jsts.index.IntervalSize;
   
   it('assumes a small interval is not zero',function(){
     expect(is.isZeroWidth((1-0.00000000000001), 1)).toBeFalsy();
--- test/spec/jsts/index/quadtree/Node.js
+++ test/spec/jsts/index/quadtree/Node.js
@@ -4,7 +4,7 @@ describe('jsts.index.quadtree.Node', function() {
   envelope = new jsts.geom.Envelope(1, 4, 1, 4);
   
   it('can be created', function() {
-    node = jsts.index.quadtree.Node.prototype.createNode(envelope);
+    node = jsts.index.quadtree.Node.createNode(envelope);
     expect(node).toBeDefined();
   });
   
