67582cf6d9a456a5a4d61f879cd9bc880de41ea4
Björn Harrtell <bjorn@wololo.org>
2011-04-13 15:24:12 +0200

    Complete port of SimpleEdgeSetIntersector

--- src/jsts/geomgraph/index/SimpleEdgeSetIntersector.js
+++ src/jsts/geomgraph/index/SimpleEdgeSetIntersector.js
@@ -5,68 +5,93 @@
  */
 
 
+
 /**
  * Finds all intersections in one or two sets of edges, using the
  * straightforward method of comparing all segments. This algorithm is too slow
  * for production use, but is useful for testing purposes.
+ *
+ * @constructor
  */
-jsts.geomgraph.index.SimpleEdgeSetIntersector {
+jsts.geomgraph.index.SimpleEdgeSetIntersector = function() {
 
 };
 
 jsts.geomgraph.index.SimpleEdgeSetIntersector.prototype = new jsts.geomgraph.index.EdgeSetIntersector();
 
 
-// TODO: finish port
-
-
 /**
-   * statistics information
-   *
-   * @type {int}
-   */
+ * statistics information
+ *
+ * @type {int}
+ */
 jsts.geomgraph.index.SimpleEdgeSetIntersector.prototype.nOverlaps = 0;
 
-jsts.geomgraph.index.SimpleEdgeSetIntersector.prototype.computeIntersections(List edges, SegmentIntersector si, boolean testAllSegments)
- {
+
+/**
+ * @param {[]}
+ *          edges
+ * @param {SegmentIntersector}
+ *          si
+ * @param {boolean}
+ *          testAllSegments
+ */
+jsts.geomgraph.index.SimpleEdgeSetIntersector.prototype.computeIntersections = function(
+    edges, si, testAllSegments) {
   this.nOverlaps = 0;
 
-  for (Iterator i0 = edges.iterator(); i0.hasNext(); ) {
-    Edge edge0 = (Edge) i0.next();
-    for (Iterator i1 = edges.iterator(); i1.hasNext(); ) {
-      Edge edge1 = (Edge) i1.next();
-      if (testAllSegments || edge0 != edge1)
-        computeIntersects(edge0, edge1, si);
+  for (var i0 = 0; i0 < edges.length; i0++) {
+    var edge0 = edges[i0];
+    for (var i1 = 0; i1 < edges.length; i1++) {
+      var edge1 = edges[i1];
+      if (testAllSegments || edge0 != edge1) {
+        this.computeIntersects(edge0, edge1, si);
+      }
     }
   }
-}
-
-
-jsts.geomgraph.index.SimpleEdgeSetIntersector.prototype.computeIntersections(List edges0, List edges1, SegmentIntersector si)
- {
-  nOverlaps = 0;
-
-  for (Iterator i0 = edges0.iterator(); i0.hasNext(); ) {
-    Edge edge0 = (Edge) i0.next();
-    for (Iterator i1 = edges1.iterator(); i1.hasNext(); ) {
-      Edge edge1 = (Edge) i1.next();
-      computeIntersects(edge0, edge1, si);
-    }
-  }
-}
+};
 
 
 /**
-   * Performs a brute-force comparison of every segment in each Edge. This has
-   * n^2 performance, and is about 100 times slower than using monotone chains.
-   */
-jsts.geomgraph.index.SimpleEdgeSetIntersector.prototype.computeIntersects(Edge e0, Edge e1, SegmentIntersector si)
- {
-  Coordinate[] pts0 = e0.getCoordinates();
-  Coordinate[] pts1 = e1.getCoordinates();
-  for (int i0 = 0; i0 < pts0.length - 1; i0++) {
-    for (int i1 = 0; i1 < pts1.length - 1; i1++) {
+ * @param {[]}
+ *          edges0
+ * @param {[]}
+ *          edges1
+ * @param {SegmentIntersector}
+ *          si
+ */
+jsts.geomgraph.index.SimpleEdgeSetIntersector.prototype.computeIntersections = function(
+    edges0, edges1, si) {
+  this.nOverlaps = 0;
+
+  for (var i0 = 0; i0 < edges0.length; i0++) {
+    var edge0 = edges0[i0];
+    for (var i1 = 0; i1 < edges1.length; i1++) {
+      var edge1 = edges1[i0];
+      this.computeIntersects(edge0, edge1, si);
+    }
+  }
+};
+
+
+/**
+ * Performs a brute-force comparison of every segment in each Edge. This has n^2
+ * performance, and is about 100 times slower than using monotone chains.
+ *
+ * @param {Edge}
+ *          e0
+ * @param {Edge}
+ *          e1
+ * @param {SegmentIntersector}
+ *          si
+ */
+jsts.geomgraph.index.SimpleEdgeSetIntersector.prototype.computeIntersects = function(
+    e0, e1, si) {
+  var pts0 = e0.getCoordinates();
+  var pts1 = e1.getCoordinates();
+  for (var i0 = 0; i0 < pts0.length - 1; i0++) {
+    for (var i1 = 0; i1 < pts1.length - 1; i1++) {
       si.addIntersections(e0, i0, e1, i1);
     }
   }
-}
+};
