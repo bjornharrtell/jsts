49a2f6dd9a5ae07c680960ba8002b3f97f936788
Björn Harrtell <bjorn@wololo.org>
2011-04-23 21:06:34 +0200

    Disable jslint for now (too slow with Rhino)

--- tools/lint.sh
+++ tools/lint.sh
@@ -1,5 +1,5 @@
 #/bin/sh
 echo "Running gjslint on source tree"
 gjslint --strict -r ../src
-echo "Running jslint on source tree"
-./jslint.py ../src
+#echo "Running jslint on source tree"
+#./jslint.py ../src
