efda41ad28b7e0845d6f0260675361fa9b7ff06c
Björn Harrtell <bjorn@wololo.org>
2011-04-25 21:19:32 +0200

    Begin Label port

--- src/jsts/geomgraph/TopologyLocation.js
+++ src/jsts/geomgraph/TopologyLocation.js
@@ -4,4 +4,33 @@
  * See /license.txt for the full text of the license.
  */
 
+
+
+/**
+ * A TopologyLocation is the labelling of a GraphComponent's topological
+ * relationship to a single Geometry.
+ * <p>
+ * If the parent component is an area edge, each side and the edge itself have a
+ * topological location. These locations are named
+ * <ul>
+ * <li> ON: on the edge
+ * <li> LEFT: left-hand side of the edge
+ * <li> RIGHT: right-hand side
+ * </ul>
+ * If the parent component is a line edge or node, there is a single topological
+ * relationship attribute, ON.
+ * <p>
+ * The possible values of a topological location are {Location.NONE,
+ * Location.EXTERIOR, Location.BOUNDARY, Location.INTERIOR}
+ * <p>
+ * The labelling is stored in an array location[j] where where j has the values
+ * ON, LEFT, RIGHT
+ *
+ * @constructor
+ */
+jsts.geomgraph.TopologyLocation = function() {
+
+};
+
+
 // TODO: port
