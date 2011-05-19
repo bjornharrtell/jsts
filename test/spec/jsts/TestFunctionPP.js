dd4e3e45620d3a2ec15ae58a1aaeb4cc350d9b5a
Björn Harrtell <bjorn@wololo.org>
2011-05-19 09:39:51 +0200

    Use jQuery instead of Ext for ajax and css selector stuff

--- test/spec/jsts/TestFunctionPP.js
+++ test/spec/jsts/TestFunctionPP.js
@@ -5,10 +5,10 @@ describe('TestFunctionPP', function() {
     return xmlLoaded; 
   };
 
-  Ext.Ajax.request({
+  $.ajax({
     url: '../testxml/general/TestFunctionPP.xml',
     success: function(response) {
-      doc = response.responseXML;
+      doc = response;
       xmlLoaded = true;
     }
   });
@@ -16,24 +16,24 @@ describe('TestFunctionPP', function() {
   it('passes all tests in TestFunctionPP.xml', function() {
     waitsFor(isReady);
     runs(function() {
-      var cases = Ext.DomQuery.select("case", doc);
+      var cases = $('case', doc);
       
       var count = 0;
       var passed = 0;
       
       for (var i = 0; i<cases.length; i++) {
         var testcase = cases[i];
-        var desc = Ext.DomQuery.select("desc", testcase)[0].textContent.trim();
-        var a = Ext.DomQuery.select("a", testcase)[0].textContent.trim();
-        var b = Ext.DomQuery.select("b", testcase)[0].textContent.trim();
+        var desc = $("desc", testcase).text().trim();
+        var a = $("a", testcase).text().trim();
+        var b = $("b", testcase).text().trim();
         
-        var tests = Ext.DomQuery.select("test", testcase);
+        var tests = $("test", testcase);
         
         for (var j = 0; j<tests.length; j++) {
           var test = tests[j];
           
-          var op = Ext.DomQuery.select("op@name", test)[0].firstChild.nodeValue;
-          var expected = Ext.DomQuery.select("op", test)[0].textContent.trim();
+          var op = $("op", test).attr('name');
+          var expected = $("op", test).text().trim();
 
           var reader = new jsts.io.WKTReader();
           var ag = reader.read(a);
--- test/spec/jsts/TestSimple.js
+++ test/spec/jsts/TestSimple.js
@@ -7,10 +7,10 @@ describe('TestSimple', function() {
     return xmlLoaded; 
   };
 
-  Ext.Ajax.request({
+  $.ajax({
     url: '../testxml/general/TestSimple.xml',
     success: function(response) {
-      doc = response.responseXML;
+      doc = response;
       xmlLoaded = true;
     }
   });
@@ -18,16 +18,16 @@ describe('TestSimple', function() {
   it('passes all tests in TestSimple.xml', function() {
     waitsFor(isReady);
     runs(function() {
-      var cases = Ext.DomQuery.select("case", doc);
+      var cases = $('case', doc);
       
       var count = 0;
       var passed = 0;
       
       for (var i = 0; i<cases.length; i++) {
         var testcase = cases[i];
-        var desc = Ext.DomQuery.select("desc", testcase)[0].textContent.trim();
-        var wkt = Ext.DomQuery.select("a", testcase)[0].textContent.trim();
-        var expected = Ext.DomQuery.select("op", testcase)[0].textContent.trim() === 'true';
+        var desc = $('desc', testcase).text();
+        var wkt = $('a', testcase).text().trim();
+        var expected = $('op', testcase).text().trim() === 'true';
         
         var reader = new jsts.io.WKTReader();
         var geometry = reader.read(wkt);
