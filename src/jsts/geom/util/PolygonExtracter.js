abb96dc080e573840dd908cb2cdc6140f1e213f2
Björn Harrtell <bjorn@wololo.org>
2011-05-19 15:48:00 +0200

    More work done for DistanceOp and deps.

---
+++ src/jsts/geom/util/PolygonExtracter.js
@@ -0,0 +1,63 @@
+/* Copyright (c) 2011 by The Authors.
+ * Published under the LGPL 2.1 license.
+ * See /license-notice.txt for the full text of the license notice.
+ * See /license.txt for the full text of the license.
+ */
+
+
+/**
+ * Extracts all the {@link Polygon} elements from a {@link Geometry}.
+ *
+ * Constructs a PolygonExtracterFilter with a list in which to store Polygons
+ * found.
+ *
+ * @param {[]}
+ *          comps
+ * @augments GeometryFilter
+ * @see GeometryExtracter
+ */
+jsts.geom.util.PolygonExtracter = function(comps) {
+  this.comps = comps;
+};
+
+
+/**
+ * @private
+ */
+jsts.geom.util.PolygonExtracter.prototype.comps = null;
+
+
+/**
+ * Extracts the {@link Polygon} elements from a single {@link Geometry} and adds
+ * them to the provided {@link List}.
+ *
+ * @param {Geometry}
+ *          geom the geometry from which to extract.
+ * @param {[]}
+ *          list the list to add the extracted elements to.
+ * @return {[]}
+ */
+jsts.geom.util.PolygonExtracter.getPolygons = function(geom, list) {
+  if (list === undefined) {
+    list = [];
+  }
+
+  if (geom instanceof jsts.geom.Polygon) {
+    list.push(geom);
+  } else if (geom instanceof jsts.geom.GeometryCollection) {
+    geom.apply(new jsts.geom.util.PolygonExtracter(list));
+  }
+  // skip non-Polygonal elemental geometries
+
+  return list;
+};
+
+
+/**
+ * @param {Geometry}
+ *          geom
+ */
+jsts.geom.util.PolygonExtracter.prototype.filter = function(geom) {
+  if (geom instanceof jsts.geom.Polygon)
+    this.comps.push(geom);
+};
---
+++ src/jsts/operation/distance/ConnectedElementLocationFilter.js
@@ -0,0 +1,58 @@
+/* Copyright (c) 2011 by The Authors.
+ * Published under the LGPL 2.1 license.
+ * See /license-notice.txt for the full text of the license notice.
+ * See /license.txt for the full text of the license.
+ */
+
+
+/**
+ * A ConnectedElementPointFilter extracts a single point from each connected
+ * element in a Geometry (e.g. a polygon, linestring or point) and returns them
+ * in a list. The elements of the list are
+ * {@link com.vividsolutions.jts.operation.distance.GeometryLocation}s.
+ *
+ * @param {[]}
+ *          locations
+ */
+jsts.operation.distance.ConnectedElementLocationFilter = function(locations) {
+  this.locations = locations;
+
+};
+
+
+/**
+ * @type {[]}
+ * @private
+ */
+jsts.operation.distance.ConnectedElementLocationFilter.prototype.locations = null;
+
+
+/**
+ * Returns a list containing a point from each Polygon, LineString, and Point
+ * found inside the specified geometry. Thus, if the specified geometry is not a
+ * GeometryCollection, an empty list will be returned. The elements of the list
+ * are {@link com.vividsolutions.jts.operation.distance.GeometryLocation}s.
+ *
+ * @param {Geometry}
+ *          geom
+ * @return {[]}
+ */
+jsts.operation.distance.ConnectedElementLocationFilter.getLocations = function(
+    geom) {
+  var locations = [];
+  geom.apply(new jsts.operation.distance.ConnectedElementLocationFilter(
+      locations));
+  return locations;
+};
+
+
+/**
+ * @param {Geometry}
+ *          geom
+ */
+jsts.operation.distance.ConnectedElementLocationFilter.prototype.filter = function(
+    geom) {
+  if (geom instanceof jsts.geom.Point || geom instanceof jsts.geom.LineString ||
+      geom instanceof jsts.geom.Polygon)
+    locations.add(new jsts.operation.distance.GeometryLocation(geom, 0, geom.getCoordinate()));
+};
