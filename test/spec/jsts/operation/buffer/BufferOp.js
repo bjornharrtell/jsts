ef34864158ff20b4d75b0dcb8d4209685a4afdee
Björn Harrtell <bjorn@wololo.org>
2011-05-31 13:26:58 +0200

    Updated from trunk and deleted extra files.

--- test/spec/jsts/operation/buffer/BufferOp.js
+++
@@ -1,6 +0,0 @@
-describe('jsts.operation.buffer.BufferOp', function() {
-  it('can be constructed', function() {
-    var bufferOp = new jsts.operation.buffer.BufferOp();
-    expect(bufferOp).toBeDefined();
-  });
-});
