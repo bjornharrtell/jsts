6bccca10c65f390df79a85ea92118bcea24e5a46
Felix Kling <sprinterl@gmx.de>
2011-06-15 21:59:51 +0800

    Added interfaces to jsts.index
    - jsts.index.ItemVisitor
    - jsts.index.SpatialIndex

---
+++ src/jsts/index/ItemVisitor.js
@@ -0,0 +1,29 @@
+/* Copyright (c) 2011 by The Authors.
+ * Published under the LGPL 2.1 license.
+ * See /license-notice.txt for the full text of the license notice.
+ * See /license.txt for the full text of the license.
+ */
+
+/**
+ * A visitor for items in an index.
+ *
+ * @version 1.7
+ */
+
+
+
+/**
+ * @interface
+ */
+jsts.index.ItemVisitor = function() {
+
+};
+
+
+/**
+ * @param {Object} item
+ * @public
+ */
+jsts.index.ItemVisitor.prototype.visitItem = function() {
+  throw new jsts.error.AbstractMethodInvocationError();
+};
