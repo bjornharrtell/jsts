aa3ec14a8574621392ddef68a49e469dc54b4b96
Felix Kling <sprinterl@gmx.de>
2011-06-25 23:16:36 +0800

    Implemented jsts.operation.union.PointGeometryUnion

--- src/jsts/operation/union/PointGeometryUnion.js
+++ src/jsts/operation/union/PointGeometryUnion.js
@@ -7,53 +7,112 @@
  * Computes the union of a {@link Puntal} geometry with
  * another arbitrary {@link Geometry}.
  * Does not copy any component geometries.
+ *
+ * @requires jsts/algorithm/PointLocator.js
+ *
  */
 
 
+
+//TODO: How do we treat Puntal?
 /**
- * @param {Puntal} pointGeom
- * @param {Geometry} otherGeom
+ * @param {jsts.geom.Puntal} pointGeom
+ * @param {jsts.geom.Geometry} otherGeom
+ * @constructor
  */
 jsts.operation.union.PointGeometryUnion = function(pointGeom, otherGeom) {
-  throw new jsts.error.NotImplementedError();
+  this.pointGeom = pointGeom;
+  this.otherGeom = otherGeom;
+  this.geomFact = otherGeom.getFactory();
 };
 
 
 /**
- * @param {Puntal} pointGeom
- * @param {Geometry} otherGeom
- * @return {Geometry}
+ * @param {jsts.geom.Puntal} pointGeom
+ * @param {jsts.geom.Geometry} otherGeom
+ * @return {jsts.geom.Geometry}
+ *
+ * @public
  */
 jsts.operation.union.PointGeometryUnion.union = function(pointGeom, otherGeom) {
-  throw new jsts.error.NotImplementedError();
+  var unioner = new jsts.operation.union.PointGeometryUnion(pointGeom, otherGeom);
+  return unioner.union();
 };
 
 
 /**
- * @type {Geometry}
+ * @type {jsts.geom.Geometry}
  * @private
  */
-jsts.operation.union.PointGeometryUnion.prototype.pointGeom;
+jsts.operation.union.PointGeometryUnion.prototype.pointGeom = null;
 
 
 /**
- * @type {Geometry}
+ * @type {jsts.geom.Geometry}
  * @private
  */
-jsts.operation.union.PointGeometryUnion.prototype.otherGeom;
+jsts.operation.union.PointGeometryUnion.prototype.otherGeom = null;
 
 
 /**
- * @type {GeometryFactory}
+ * @type {jsts.geom.GeometryFactory}
  * @private
  */
-jsts.operation.union.PointGeometryUnion.prototype.geomFact;
+jsts.operation.union.PointGeometryUnion.prototype.geomFact = null;
 
 
 /**
  *
- * @return {Geometry}
+ * @return {jsts.geom.Geometry}
  */
 jsts.operation.union.PointGeometryUnion.prototype.union = function() {
-  throw new jsts.error.NotImplementedError();
+  var locator = new jsts.algorithm.PointLocator();
+  // use a set to eliminate duplicates, as required for union
+  // Should be a tree set. So we have to check for uniqueness and sort it.
+  var exteriorCoords = [];
+
+  for (var i = 0, l = this.pointGeom.getNumGeometries(); i < l; i++) {
+    var point = this.pointGeom.getGeometryN(i);
+    var coord = point.getCoordinate();
+    var loc = locator.locate(coord, this.otherGeom);
+
+    if (loc === jsts.geom.Location.EXTERIOR) {
+
+      // TreeSet: check for uniqueness
+      var include = true;
+      for (var j = exteriorCoords.length; i--;) {
+        if (exteriorCoords[j].equals(coord)) {
+          include = false;
+          break;
+        }
+      }
+
+      if (include) {
+        exteriorCoords.push(coord);
+      }
+    }
+  }
+
+  //TreeSet: sort ascending
+  exteriorCoords.sort(function(x, y) {
+    return x.compareTo(y);
+  });
+
+  //if no points are in exterior, return the other geom
+  if (exteriorCoords.length === 0) {
+    return this.otherGeom;
+  }
+
+  //make a puntal geometry of appropriate size
+  var ptComp = null;
+  var coords = jsts.geom.CoordinateArrays.toCoordinateArray(exteriorCoords);
+  if (coords.length === 1) {
+    ptComp = this.geomFact.createPoint(coords[0]);
+  }
+  else {
+    ptComp = this.geomFact.createMultiPoint(coords);
+  }
+
+  //add point component to the other geometry
+  return jsts.geom.util.GeometryCombiner.combine(ptComp, this.otherGeom);
 };
