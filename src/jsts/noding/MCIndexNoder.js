59c334476c7348c96ffc8352d5b8e7234e3e39d6
Björn Harrtell <bjorn@wololo.org>
2011-08-06 11:09:01 +0200

    Refactor some classes to new style.

--- src/jsts/noding/MCIndexNoder.js
+++ src/jsts/noding/MCIndexNoder.js
@@ -4,137 +4,145 @@
  * See /license.txt for the full text of the license.
  */
 
-/**
- * @requires jsts/noding/SinglePassNoder.js
- */
+(function() {
 
-/**
- * @constructor
- */
-jsts.noding.MCIndexNoder = function() {
-  this.monoChains = [];
-  this.index = new jsts.index.strtree.STRtree();
-};
+  /**
+   * @requires jsts/noding/SinglePassNoder.js
+   * @requires jsts/index/strtree/STRtree.js
+   * @requires jsts/noding/NodedSegmentString.js
+   * @requires jsts/index.chain/MonotoneChainBuilder.js
+   * @requires jsts/index/chain/MonotoneChainOverlapAction.js
+   */
 
-jsts.noding.MCIndexNoder.prototype = new jsts.noding.SinglePassNoder();
-jsts.noding.MCIndexNoder.constructor = jsts.noding.MCIndexNoder;
-
-/**
- * @type {Array}
- * @private
- */
-jsts.noding.MCIndexNoder.prototype.monoChains = null;
-/**
- * @type {SpatialIndex}
- * @private
- */
-jsts.noding.MCIndexNoder.prototype.index = null;
-/**
- * @type {number}
- * @private
- */
-jsts.noding.MCIndexNoder.prototype.idCounter = 0;
-
-/**
- * @type {Array}
- * @private
- */
-jsts.noding.MCIndexNoder.prototype.nodedSegStrings = null;
-/**
- * statistics
- *
- * @type {number}
- * @private
- */
-jsts.noding.MCIndexNoder.prototype.nOverlaps = 0;
+  var MonotoneChainOverlapAction = jsts.index.chain.MonotoneChainOverlapAction;
+  var SinglePassNoder = jsts.noding.SinglePassNoder;
+  var STRtree = jsts.index.strtree.STRtree;
+  var NodedSegmentString = jsts.noding.NodedSegmentString;
+  var MonotoneChainBuilder = jsts.index.chain.MonotoneChainBuilder;
 
 
-jsts.noding.MCIndexNoder.prototype.getMonotoneChains = function() {
-  return this.monoChains;
-};
+  /**
+   * @constructor
+   * @private
+   */
+  SegmentOverlapAction = function(si) {
+    this.si = si;
 
-jsts.noding.MCIndexNoder.prototype.getIndex = function() {
-  return this.index;
-};
+  };
+  SegmentOverlapAction.prototype = new MonotoneChainOverlapAction();
+  SegmentOverlapAction.constructor = SegmentOverlapAction;
 
-jsts.noding.MCIndexNoder.prototype.getNodedSubstrings = function() {
-  return jsts.noding.NodedSegmentString
-      .getNodedSubstrings(this.nodedSegStrings);
-};
+  /**
+   * @type {SegmentIntersector}
+   * @private
+   */
+  SegmentOverlapAction.prototype.si = null;
 
-jsts.noding.MCIndexNoder.prototype.computeNodes = function(inputSegStrings) {
-  this.nodedSegStrings = inputSegStrings;
-  for (var i = 0; i < inputSegStrings.length; i++) {
-    this.add(inputSegStrings[i]);
-  }
-  this.intersectChains();
-};
+  SegmentOverlapAction.prototype.overlap = function(mc1, start1, mc2, start2) {
+    var ss1 = mc1.getContext();
+    var ss2 = mc2.getContext();
+    this.si.processIntersections(ss1, start1, ss2, start2);
+  };
 
-/**
- * @private
- */
-jsts.noding.MCIndexNoder.prototype.intersectChains = function() {
-  var overlapAction = new jsts.noding.MCIndexNoder.SegmentOverlapAction(
-      this.segInt);
+  /**
+   * @constructor
+   */
+  jsts.noding.MCIndexNoder = function() {
+    this.monoChains = [];
+    this.index = new STRtree();
+  };
 
-  for (var i = 0; i < this.monoChains.length; i++) {
-    var queryChain = this.monoChains[i];
-    var overlapChains = this.index.query(queryChain.getEnvelope());
-    for (var j = 0; j < overlapChains.length; j++) {
-      var testChain = overlapChains[j];
-      /**
-       * following test makes sure we only compare each pair of chains once and
-       * that we don't compare a chain to itself
-       */
-      if (testChain.getId() > queryChain.getId()) {
-        queryChain.computeOverlaps(testChain, overlapAction);
-        this.nOverlaps++;
-      }
-      // short-circuit if possible
-      if (this.segInt.isDone())
-        return;
+  jsts.noding.MCIndexNoder.prototype = new SinglePassNoder();
+  jsts.noding.MCIndexNoder.constructor = jsts.noding.MCIndexNoder;
+
+  /**
+   * @type {Array}
+   * @private
+   */
+  jsts.noding.MCIndexNoder.prototype.monoChains = null;
+  /**
+   * @type {SpatialIndex}
+   * @private
+   */
+  jsts.noding.MCIndexNoder.prototype.index = null;
+  /**
+   * @type {number}
+   * @private
+   */
+  jsts.noding.MCIndexNoder.prototype.idCounter = 0;
+
+  /**
+   * @type {Array}
+   * @private
+   */
+  jsts.noding.MCIndexNoder.prototype.nodedSegStrings = null;
+  /**
+   * statistics
+   *
+   * @type {number}
+   * @private
+   */
+  jsts.noding.MCIndexNoder.prototype.nOverlaps = 0;
+
+
+  jsts.noding.MCIndexNoder.prototype.getMonotoneChains = function() {
+    return this.monoChains;
+  };
+
+  jsts.noding.MCIndexNoder.prototype.getIndex = function() {
+    return this.index;
+  };
+
+  jsts.noding.MCIndexNoder.prototype.getNodedSubstrings = function() {
+    return NodedSegmentString.getNodedSubstrings(this.nodedSegStrings);
+  };
+
+  jsts.noding.MCIndexNoder.prototype.computeNodes = function(inputSegStrings) {
+    this.nodedSegStrings = inputSegStrings;
+    for (var i = 0; i < inputSegStrings.length; i++) {
+      this.add(inputSegStrings[i]);
     }
-  }
-};
+    this.intersectChains();
+  };
 
-/**
- * @private
- */
-jsts.noding.MCIndexNoder.prototype.add = function(segStr) {
-  var segChains = jsts.index.chain.MonotoneChainBuilder.getChains(segStr
-      .getCoordinates(), segStr);
-  for (var i = 0; i < segChains.length; i++) {
-    var mc = segChains[i];
-    mc.setId(this.idCounter++);
-    this.index.insert(mc.getEnvelope(), mc);
-    this.monoChains.push(mc);
-  }
-};
+  /**
+   * @private
+   */
+  jsts.noding.MCIndexNoder.prototype.intersectChains = function() {
+    var overlapAction = new SegmentOverlapAction(this.segInt);
 
-/**
- * @requires jsts/index/chain/MonotoneChainOverlapAction.js
- */
+    for (var i = 0; i < this.monoChains.length; i++) {
+      var queryChain = this.monoChains[i];
+      var overlapChains = this.index.query(queryChain.getEnvelope());
+      for (var j = 0; j < overlapChains.length; j++) {
+        var testChain = overlapChains[j];
+        /**
+         * following test makes sure we only compare each pair of chains once
+         * and that we don't compare a chain to itself
+         */
+        if (testChain.getId() > queryChain.getId()) {
+          queryChain.computeOverlaps(testChain, overlapAction);
+          this.nOverlaps++;
+        }
+        // short-circuit if possible
+        if (this.segInt.isDone())
+          return;
+      }
+    }
+  };
 
-/**
- * constructor
- */
-jsts.noding.MCIndexNoder.SegmentOverlapAction = function(si) {
-  this.si = si;
+  /**
+   * @private
+   */
+  jsts.noding.MCIndexNoder.prototype.add = function(segStr) {
+    var segChains = MonotoneChainBuilder.getChains(segStr.getCoordinates(),
+        segStr);
+    for (var i = 0; i < segChains.length; i++) {
+      var mc = segChains[i];
+      mc.setId(this.idCounter++);
+      this.index.insert(mc.getEnvelope(), mc);
+      this.monoChains.push(mc);
+    }
+  };
 
-};
-jsts.noding.MCIndexNoder.SegmentOverlapAction.prototype = new jsts.index.chain.MonotoneChainOverlapAction();
-jsts.noding.MCIndexNoder.SegmentOverlapAction.constructor = jsts.noding.MCIndexNoder.SegmentOverlapAction;
-
-/**
- * @type {SegmentIntersector}
- * @private
- */
-jsts.noding.MCIndexNoder.SegmentOverlapAction.prototype.si = null;
-
-
-jsts.noding.MCIndexNoder.SegmentOverlapAction.prototype.overlap = function(mc1,
-    start1, mc2, start2) {
-  var ss1 = mc1.getContext();
-  var ss2 = mc2.getContext();
-  this.si.processIntersections(ss1, start1, ss2, start2);
-};
+}());
