cd5dd8263dacd3629d8f9210378577e151996604
Björn Harrtell <bjorn@wololo.org>
2011-08-07 14:39:00 +0200

    Refactored EdgeEnd.

--- src/jsts/geomgraph/EdgeEnd.js
+++ src/jsts/geomgraph/EdgeEnd.js
@@ -4,151 +4,199 @@
  * See /license.txt for the full text of the license.
  */
 
+(function() {
 
+  /**
+   * @requires jsts/util/Assert.js
+   */
 
-/**
- * Models the end of an edge incident on a node.
- * EdgeEnds have a direction
- * determined by the direction of the ray from the initial
- * point to the next point.
- * EdgeEnds are comparable under the ordering
- * "a has a greater angle with the x-axis than b".
- * This ordering is used to sort EdgeEnds around a node.
- * @param {Edge} edge
- * @param {Coordinate} p0
- * @param {Coordinate} p1
- * @param {Label} label
- * @constructor
- */
-jsts.geomgraph.EdgeEnd = function(edge,  p0,  p1,  label) {
-  this.edge = edge;
-  if (p0 !== undefined) {
-    if (p1 === undefined) {
-      throw new jsts.error.IllegalArgumentError('expected two coords');
+  var Assert = jsts.util.Assert;
+
+  /**
+   * Models the end of an edge incident on a node. EdgeEnds have a direction
+   * determined by the direction of the ray from the initial point to the next
+   * point. EdgeEnds are comparable under the ordering "a has a greater angle
+   * with the x-axis than b". This ordering is used to sort EdgeEnds around a
+   * node.
+   *
+   * @param {Edge}
+   *          edge
+   * @param {Coordinate}
+   *          p0
+   * @param {Coordinate}
+   *          p1
+   * @param {Label}
+   *          label
+   * @constructor
+   */
+  var EdgeEnd = function(edge, p0, p1, label) {
+    this.edge = edge;
+    if (p0 !== undefined) {
+      if (p1 === undefined) {
+        throw new jsts.error.IllegalArgumentError('expected two coords');
+      }
+      this.init(p0, p1);
+      this.label = label;
     }
-    this.init(p0, p1);
-    this.label = label;
-  }
-};
+  };
 
 
-/**
- * the parent edge of this edge end
- * @type {Edge}
- * @protected
- */
-jsts.geomgraph.EdgeEnd.prototype.edge = null;
+  /**
+   * the parent edge of this edge end
+   *
+   * @type {Edge}
+   * @protected
+   */
+  EdgeEnd.prototype.edge = null;
 
 
-/**
+  /**
    * @type {Label}
    * @protected
    */
-jsts.geomgraph.EdgeEnd.prototype.label = null;
+  EdgeEnd.prototype.label = null;
 
 
-/**
- * the node this edge end originates at
- * @type {Node}
- * @private
- */
-jsts.geomgraph.EdgeEnd.prototype.node = null;
+  /**
+   * the node this edge end originates at
+   *
+   * @type {Node}
+   * @private
+   */
+  EdgeEnd.prototype.node = null;
 
 
-/**
+  /**
    * points of initial line segment
+   *
    * @type {Coordinate}
    * @private
    */
-jsts.geomgraph.EdgeEnd.prototype.p0 = null;
-jsts.geomgraph.EdgeEnd.prototype.p1 = null;
+  EdgeEnd.prototype.p0 = null;
+  EdgeEnd.prototype.p1 = null;
 
 
-/**
+  /**
    * the direction vector for this edge from its starting point
+   *
    * @type {double}
    * @private
    */
-jsts.geomgraph.EdgeEnd.prototype.dx = null;
-jsts.geomgraph.EdgeEnd.prototype.dy = null;
+  EdgeEnd.prototype.dx = null;
+  EdgeEnd.prototype.dy = null;
 
 
-/**
+  /**
    * @type {int}
    * @private
    */
-jsts.geomgraph.EdgeEnd.prototype.quadrant = null;
+  EdgeEnd.prototype.quadrant = null;
 
 
-/**
-   * @param {Coordinate} p0
-   * @param {Coordinate} p1
+  /**
+   * @param {Coordinate}
+   *          p0
+   * @param {Coordinate}
+   *          p1
    * @protected
    */
-jsts.geomgraph.EdgeEnd.prototype.init = function(p0,  p1)  {
-  this.p0 = p0;
-  this.p1 = p1;
-  this.dx = p1.x - p0.x;
-  this.dy = p1.y - p0.y;
-  this.quadrant = jsts.geomgraph.Quadrant.quadrant(this.dx, this.dy);
-  // TODO: Assert.isTrue(! (dx == 0 && dy == 0), 'EdgeEnd with identical endpoints found');
-};
+  EdgeEnd.prototype.init = function(p0, p1) {
+    this.p0 = p0;
+    this.p1 = p1;
+    this.dx = p1.x - p0.x;
+    this.dy = p1.y - p0.y;
+    this.quadrant = jsts.geomgraph.Quadrant.quadrant(this.dx, this.dy);
+    TODO: Assert.isTrue(!(this.dx === 0 && this.dy === 0),
+        'EdgeEnd with identical endpoints found');
+  };
 
-jsts.geomgraph.EdgeEnd.prototype.getEdge = function() { return this.edge; };
+  EdgeEnd.prototype.getEdge = function() {
+    return this.edge;
+  };
 
-jsts.geomgraph.EdgeEnd.prototype.getLabel = function() { return this.label; };
+  EdgeEnd.prototype.getLabel = function() {
+    return this.label;
+  };
 
-jsts.geomgraph.EdgeEnd.prototype.getCoordinate = function() { return this.p0; };
+  EdgeEnd.prototype.getCoordinate = function() {
+    return this.p0;
+  };
 
-jsts.geomgraph.EdgeEnd.prototype.getDirectedCoordinate = function() { return this.p1; };
+  EdgeEnd.prototype.getDirectedCoordinate = function() {
+    return this.p1;
+  };
 
-jsts.geomgraph.EdgeEnd.prototype.getQuadrant = function() { return this.quadrant; };
+  EdgeEnd.prototype.getQuadrant = function() {
+    return this.quadrant;
+  };
 
-jsts.geomgraph.EdgeEnd.prototype.getDx = function() { return this.dx; };
+  EdgeEnd.prototype.getDx = function() {
+    return this.dx;
+  };
 
-jsts.geomgraph.EdgeEnd.prototype.getDy = function() { return this.dy; };
-
-
-jsts.geomgraph.EdgeEnd.prototype.setNode = function(node) { this.node = node; };
-
-jsts.geomgraph.EdgeEnd.prototype.getNode = function() { return this.node; };
-
-jsts.geomgraph.EdgeEnd.prototype.compareTo = function(e)  {
-  return this.compareDirection(e);
-};
+  EdgeEnd.prototype.getDy = function() {
+    return this.dy;
+  };
 
 
-/**
+  EdgeEnd.prototype.setNode = function(node) {
+    this.node = node;
+  };
+
+  EdgeEnd.prototype.getNode = function() {
+    return this.node;
+  };
+
+  EdgeEnd.prototype.compareTo = function(e) {
+    return this.compareDirection(e);
+  };
+
+
+  /**
    * Implements the total order relation:
    * <p>
-   *    a has a greater angle with the positive x-axis than b
+   * a has a greater angle with the positive x-axis than b
    * <p>
    * Using the obvious algorithm of simply computing the angle is not robust,
-   * since the angle calculation is obviously susceptible to roundoff.
-   * A robust algorithm is:
-   * - first compare the quadrant.  If the quadrants
-   * are different, it it trivial to determine which vector is "greater".
-   * - if the vectors lie in the same quadrant, the computeOrientation function
-   * can be used to decide the relative orientation of the vectors.
-   * @param {EdgeEnd} e
+   * since the angle calculation is obviously susceptible to roundoff. A robust
+   * algorithm is: - first compare the quadrant. If the quadrants are different,
+   * it it trivial to determine which vector is "greater". - if the vectors lie
+   * in the same quadrant, the computeOrientation function can be used to decide
+   * the relative orientation of the vectors.
+   *
+   * @param {EdgeEnd}
+   *          e
    * @return {number}
    */
-jsts.geomgraph.EdgeEnd.prototype.compareDirection = function(e)  {
-  if (this.dx === e.dx && this.dy === e.dy)
-    return 0;
-  // if the rays are in different quadrants, determining the ordering is trivial
-  if (this.quadrant > e.quadrant) return 1;
-  if (this.quadrant < e.quadrant) return -1;
-  // vectors are in the same quadrant - check relative orientation of direction vectors
-  // this is > e if it is CCW of e
-  return jsts.algorithm.CGAlgorithms.computeOrientation(e.p0, e.p1, this.p1);
-};
+  EdgeEnd.prototype.compareDirection = function(e) {
+    if (this.dx === e.dx && this.dy === e.dy)
+      return 0;
+    // if the rays are in different quadrants, determining the ordering is
+    // trivial
+    if (this.quadrant > e.quadrant)
+      return 1;
+    if (this.quadrant < e.quadrant)
+      return -1;
+    // vectors are in the same quadrant - check relative orientation of
+    // direction vectors
+    // this is > e if it is CCW of e
+    return jsts.algorithm.CGAlgorithms.computeOrientation(e.p0, e.p1, this.p1);
+  };
 
-jsts.geomgraph.EdgeEnd.prototype.computeLabel = function(boundaryNodeRule)  {
+  EdgeEnd.prototype.computeLabel = function(boundaryNodeRule) {
   // subclasses should override this if they are using labels
-};
+  };
 
-jsts.geomgraph.EdgeEnd.prototype.toString = function() {
-  // TODO: this hashcode might not be equivalent to JTS code, so maps containg edgeends could be a cause of bugs
-  return 'EdgeEnd_' + this.dx + '_' + this.dy + '_' + this.quadrant;
-};
+  /**
+   * Need to be representable by string for use as key in Maps. TODO: Might not
+   * be equivalent to JTS/Java, need investigation.
+   */
+  EdgeEnd.prototype.toString = function() {
+    // TODO: this hashcode might not be equivalent to JTS code, so maps containg
+    // edgeends could be a cause of bugs
+    return 'EdgeEnd_' + this.dx + '_' + this.dy + '_' + this.quadrant;
+  };
+
+  jsts.geomgraph.EdgeEnd = EdgeEnd;
+
+})();
