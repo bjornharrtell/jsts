97e7b7656442cedebb2393a6bb2565a892915bee
Björn Harrtell <bjorn@wololo.org>
2011-08-13 14:13:41 +0200

    Initial ConvexHull port.

---
+++ src/jsts/util/UniqueCoordinateArrayFilter.js
@@ -0,0 +1,54 @@
+/* Copyright (c) 2011 by The Authors.
+ * Published under the LGPL 2.1 license.
+ * See /license-notice.txt for the full text of the license notice.
+ * See /license.txt for the full text of the license.
+ */
+
+(function() {
+
+  /**
+   * @requires jsts/geom/CoordinateFilter.js
+   */
+
+  var ArrayList = javascript.util.ArrayList;
+  var TreeSet = javascript.util.TreeSet;
+
+  var CoordinateFilter = jsts.geom.CoordinateFilter;
+
+  /**
+   * A {@link CoordinateFilter} that builds a set of <code>Coordinate</code>s.
+   * The set of coordinates contains no duplicate points.
+   *
+   * @constructor
+   */
+  var UniqueCoordinateArrayFilter = function() {
+    this.treeSet = new TreeSet();
+    this.list = new ArrayList();
+  };
+
+
+  UniqueCoordinateArrayFilter.prototype = new CoordinateFilter();
+
+  UniqueCoordinateArrayFilter.prototype.treeSet = null;
+  UniqueCoordinateArrayFilter.prototype.list = null;
+
+
+  /**
+   * Returns the gathered <code>Coordinate</code>s.
+   *
+   * @return the <code>Coordinate</code>s collected by this
+   *         <code>CoordinateArrayFilter.</code>
+   */
+  UniqueCoordinateArrayFilter.prototype.getCoordinates = function() {
+    return list.toArray();
+  };
+
+  UniqueCoordinateArrayFilter.prototype.filter = function(coord) {
+    if (!treeSet.contains(coord)) {
+      list.add(coord);
+      treeSet.add(coord);
+    }
+  };
+
+  jsts.util.UniqueCoordinateArrayFilter = UniqueCoordinateArrayFilter;
+})();
