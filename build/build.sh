fedca5e783b6fb77235ff215b20f496e0f82b62e
Björn Harrtell <bjorn@wololo.org>
2011-08-16 13:11:21 +0200

    Created release build script. Added changelog.

--- build/build.sh
+++ build/build.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 jsbuild full.cfg -v -o ../lib -j jsts.js
+tar -cvf jsts-0.9.0.tar ../src ../doc ../lib ../examples ../src ../license.txt ../authors.txt ../README.md
+gzip jsts-0.9.0.tar
+mkdir tmp
+cd tmp
+tar xvfz ../jsts-0.9.0.tar.gz
+zip ../jsts-0.9.0.zip * -r
+cd ..
+rm tmp -rf
 
