6f42a2b7e021b2206e30f32f7ba91b82058369e7
Björn Harrtell <bjorn@wololo.org>
2011-08-20 14:01:25 +0200

    Update ChangeLog for 0.9.1 release.

--- build/build.sh
+++ build/build.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 jsbuild full.cfg -v -o ../lib -j jsts.js
-tar -cvf jsts-0.9.1.tar ../src ../doc ../lib ../examples ../src ../license.txt ../authors.txt ../README.md
-rm jsts-0.9.0.tar.gz
+tar -cvf jsts-0.9.1.tar ../src ../doc ../lib ../examples ../src ../license.txt ../authors.txt ../ChangeLog ../README.md
+rm jsts-0.9.1.tar.gz
 gzip -9 jsts-0.9.1.tar
 mkdir tmp
 cd tmp
 tar xvfz ../jsts-0.9.1.tar.gz
-rm ../jsts-0.9.0.zip
+rm ../jsts-0.9.1.zip
 zip -r -9 ../jsts-0.9.1.zip *
 cd ..
 rm tmp -rf
-
