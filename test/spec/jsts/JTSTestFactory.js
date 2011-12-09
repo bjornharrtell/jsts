b26021d35f7a3a308858bd5e2878f703797cccfb
Björn Harrtell <bjorn@wololo.org>
2011-12-09 21:55:44 +0100

    Cleaned up and fixed new test impl. (closes #80)

--- test/spec/jsts/JTSTestFactory.js
+++ test/spec/jsts/JTSTestFactory.js
@@ -1,4 +1,8 @@
 JTSTestFactory = {};
+
+/**
+ * Translate JTS XML testcase document to Jasmine suites
+ */
 JTSTestFactory.generate = function(doc, title) {
   var cases = $('case', doc);
   var precisionModelInfo = $('precisionModel', doc);
@@ -14,6 +18,71 @@ JTSTestFactory.generate = function(doc, title) {
     }
   }
 
+  var reader = new jsts.io.WKTReader(geometryFactory);
+
+  /**
+   * Translate JTS XML "test" to a Jasmine spec
+   */
+  var generateSpec = function(a, b, opname, arg2, arg3, expected) {
+    it('Executing ' + opname + ' on test geometry', function() {
+
+      var inputs = ' Input geometry A: ' + a + (b ? ' B: ' + b : '');
+
+      var result;
+
+      // fix opnames to real methods where needed
+      if (opname === 'convexhull')
+        opname = 'convexHull';
+      else if (opname === 'getboundary')
+        opname = 'getBoundary';
+      else if (opname === 'symdifference')
+        opname = 'symDifference';
+
+      // switch execution logic depending on opname
+      if (opname === 'buffer') {
+        result = a[opname](parseFloat(arg2));
+      } else if (opname === 'getCentroid') {
+        result = a[opname]();
+      } else {
+        result = a[opname](b, arg3);
+      }
+
+      // switch comparison logic depending on opname
+      // TODO: should be a cleaner approach...
+      if (opname === 'relate' || opname === 'contains' ||
+          opname === 'intersects' || opname === 'equalsExact' ||
+          opname === 'equalsNorm' || opname === 'isSimple') {
+        var expectedBool = expected === 'true';
+        if (expectedBool !== result) {
+          throw new Error('Result: ' + result + ' Expected: ' + expected +
+              inputs);
+        } else {
+          expect(true).toBeTruthy();
+        }
+      } else if (opname === 'distance') {
+        var expectedDistance = parseFloat(expected);
+        if (result !== expectedDistance) {
+          throw new Error('Result: ' + result + ' Expected: ' +
+              parseFloat(expectedDistance) + inputs);
+        } else {
+          expect(true).toBeTruthy();
+        }
+      } else {
+        var expectedGeometry = reader.read(expected);
+        result.normalize();
+        expectedGeometry.normalize();
+
+        if (result.compareTo(expectedGeometry) !== 0) {
+          throw new Error('Result: ' + result + ' Expected: ' +
+              expectedGeometry + inputs);
+        } else {
+          expect(true).toBeTruthy();
+        }
+      }
+
+    });
+  };
+
   for ( var i = 0; i < cases.length; i++) {
     var testcase = cases[i];
     var desc = $("desc", testcase).text().trim();
@@ -24,6 +93,7 @@ JTSTestFactory.generate = function(doc, title) {
       var tests = $("test", testcase);
 
       for ( var j = 0; j < tests.length; j++) {
+
         var test = tests[j];
 
         var opname = $("op", test).attr('name');
@@ -31,66 +101,12 @@ JTSTestFactory.generate = function(doc, title) {
         var arg3 = $("op", test).attr('arg3');
         var expected = $("op", test).text().trim().replace(/\n/g, '');
 
-        var reader = new jsts.io.WKTReader(geometryFactory);
         var a = reader.read(awkt);
         var b = reader.read(bwkt);
 
-        var inputs = ' Input geometry A: ' + a + (b ? ' B: ' + b : '');
-
-        // fix opnames to real methods where needed
-        if (opname === 'convexhull')
-          opname = 'convexHull';
-        else if (opname === 'getboundary')
-          opname = 'getBoundary';
-        else if (opname === 'symdifference')
-          opname = 'symDifference';
-
-        it('Executing ' + opname + ' on test geometry', function() {
-          var result;
-
-          // switch execution logic depending on opname
-          if (opname === 'buffer') {
-            result = a[opname](arg2);
-          } else if (opname === 'getCentroid') {
-            result = a[opname]();
-          } else {
-            result = a[opname](b, arg3);
-          }
-
-          // switch comparison logic depending on opname
-          if (opname === 'relate' || opname === 'contains' || opname === 'intersects' ||
-              opname === 'equalsExact' || opname === 'equalsNorm' ||
-              opname === 'isSimple') {
-            var expectedBool = expected === 'true';
-            if (expectedBool !== result) {
-              throw new Error('Result: ' + result + ' Expected: ' + expected +
-                  inputs);
-            } else {
-              expect(true).toBeTruthy();
-            }
-          } else if (opname === 'distance') {
-            var expectedDistance = parseFloat(expected);
-            if (result !== expectedDistance) {
-              throw new Error('Result: ' + result + ' Expected: ' + parseFloat(expectedDistance) +
-                  inputs);
-            } else {
-              expect(true).toBeTruthy();
-            }
-          } else {
-            var expectedGeometry = reader.read(expected);
-            result.normalize();
-            expectedGeometry.normalize();
-
-            if (result.compareTo(expectedGeometry) !== 0) {
-              throw new Error('Result: ' + result + ' Expected: ' + expectedGeometry +
-                  inputs);
-            } else {
-              expect(true).toBeTruthy();
-            }
-          }
-
-        });
+        generateSpec(a, b, opname, arg2, arg3, expected);
       }
     });
   }
+  
 };
