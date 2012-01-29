ad307b45ad0d15b28dd75fd85ae5bb25da24faed
Steven Looman <steven.looman@gmail.com>
2012-01-29 19:58:50 +0100

    More work

--- src/jsts/io/GeoJSONParser.js
+++ src/jsts/io/GeoJSONParser.js
@@ -18,43 +18,41 @@
             obj = json;
         }
 
-        return this.parse.geometry.apply(this, [ obj ]);
+        var type = obj.type.toLowerCase();
+
+        if (!this.parse[type]) {
+            throw new Error('Unknown GeoJSON type: ' + obj.type);
+        }
+
+        if (this.geometryTypes.indexOf(type) != -1) {
+            return this.parse[type].apply(this, [ obj.coordinates ]);
+        } else if (type === 'geometrycollection') {
+            return this.parse[type].apply(this, [ obj.geometries ]);
+        }
+
+        // feature or feature collection
+        return this.parse[type].apply(this, [ obj ]);
     };
 
     jsts.io.GeoJSONParser.prototype.parse = {
-        // entry functions
-        'geometry': function(obj) {
-            var type = obj.type.toLowerCase();
-
-            if (!this.parse[type]) {
-                throw new Error('Unknown GeoJSON type: ' + obj.type);
-            }
-
-            if (this.geometryTypes.indexOf(type) != -1) {
-                return this.parse[type].apply(this, [ obj.coordinates ]);
-            } else if (type === 'geometrycollection') {
-                return this.parse[type].apply(this, [ obj.geometries ]);
-            }
-
-            // feature or feature collection
-            return this.parse[type].apply(this, [ obj ]);
-        },
-
         'feature': function(obj) {
             var feature = {};
+
             // copy features
             for (var key in obj) {
                 feature[key] = obj[key];
             }
+
             // parse geometry
             if (obj.geometry) {
                 var type = obj.geometry.type.toLowerCase();
                 if (!this.parse[type]) {
                     throw new Error('Unknown GeoJSON type: ' + obj.type);
                 }
-                feature.geometry = this.parse.geometry.apply(this, [ obj.geometry ]);
+                feature.geometry = this.read(obj.geometry);
             }
-            // bbox?
+
+            // bbox
             if (obj.bbox) {
                 feature.bbox = this.parse.bbox.apply(this, [ obj.bbox ]);
             }
@@ -69,14 +67,13 @@
                 featureCollection.features = [];
 
                 for (var i = 0; i < obj.features.length; ++i) {
-                    featureCollection.features.push(this.parse.geometry.apply(this, [ obj.features[i] ]));
+                    featureCollection.features.push(this.read(obj.features[i]));
                 }
             }
 
             if (obj.bbox) {
                 featureCollection.bbox = this.parse.bbox.apply(this, [ obj.bbox ]);
             }
-
 
             return featureCollection;
         },
@@ -85,10 +82,12 @@
         // utility functions
         'coordinates': function(array) {
             var coordinates = [];
+
             for (var i = 0; i < array.length; ++i) {
                 var sub = array[i];
                 coordinates.push(new jsts.geom.Coordinate(sub[0], sub[1]));
             }
+
             return coordinates;
         },
 
@@ -111,9 +110,11 @@
 
         'multipoint': function(array) {
             var points = [];
+
             for (var i = 0; i < array.length; ++i) {
                 points.push(this.parse.point.apply(this, [ array[i] ]));
             }
+
             return this.geometryFactory.createMultiPoint(points);
         },
 
@@ -124,9 +125,11 @@
 
         'multilinestring': function(array) {
             var lineStrings = [];
+
             for (var i = 0; i < array.length; ++i) {
                 lineStrings.push(this.parse.linestring.apply(this, [ array[i] ]));
             }
+
             return this.geometryFactory.createMultiLineString(lineStrings);
         },
 
@@ -149,19 +152,23 @@
 
         'multipolygon': function(array) {
             var polygons = [];
+
             for (var i = 0; i < array.length; ++i) {
                 var polygon = array[i];
                 polygons.push(this.parse.polygon.apply(this, [ polygon ]));
             }
+
             return this.geometryFactory.createMultiPolygon(polygons);
         },
 
         'geometrycollection': function(array) {
             var geometries = [];
+
             for (var i = 0; i < array.length; ++i) {
                 var geometry = array[i];
-                geometries.push(this.parse.geometry.apply(this, [ geometry ]));
+                geometries.push(this.read(geometry));
             }
+
             return this.geometryFactory.createGeometryCollection(geometries);
         }
     };
@@ -182,14 +189,14 @@
 
     jsts.io.GeoJSONParser.prototype.extract = {
         'geometry': function(geometry) {
-            var type = geometry.CLASS_NAME.split('.')[2].toLowerCase();
-            if (!this.extract[type]) {
+            var type = geometry.CLASS_NAME.split('.')[2];
+            if (!this.extract[type.toLowerCase()]) {
                 return null;
             }
 
             return {
-                'type': type[0].toUpperCase() + type.slice(1),
-                'geometry': this.extract[type].apply(this, [ geometry ])
+                'type': type,
+                'geometry': this.extract[type.toLowerCase()].apply(this, [ geometry ])
             };
         },
 
@@ -205,56 +212,68 @@
 
         'multipoint': function(multipoint) {
             var array = [];
+
             for (var i = 0; i < multipoint.geometries.length; ++i) {
                 var point = multipoint.geometries[i];
                 array.push(this.extract.point.apply(this, [ point ]));
             }
+
             return array;
         },
 
         'linestring': function(linestring) {
             var array = [];
+
             for (var i = 0; i < linestring.points.length; ++i) {
                 var coordinate = linestring.points[i];
                 array.push(this.extract.coordinate.apply(this, [ coordinate ]));
             }
+
             return array;
         },
 
         'multilinestring': function(multilinestring) {
             var array = [];
+
             for (var i = 0; i < multilinestring.geometries.length; ++i) {
                 var linestring = multilinestring.geometries[i];
                 array.push(this.extract.linestring.apply(this, [ linestring ]));
             }
+
             return array;
         },
 
         'polygon': function(polygon) {
             var array = [];
+
             array.push(this.extract.linestring.apply(this, [ polygon.shell ]));
             for (var i = 0; i < polygon.holes.length; ++i) {
                 var hole = polygon.holes[i];
                 array.push(this.extract.linestring.apply(this, [ hole ]));
             }
+
             return array;
         },
 
         'multipolygon': function(multipolygon) {
             var array = [];
+
             for (var i = 0; i < multipolygon.geometries.length; ++i) {
                 var polygon = multipolygon.geometries[i];
                 array.push(this.extract.polygon.apply(this, [ polygon ]));
             }
+
             return array;
         },
 
         'geometrycollection': function(collection) {
             var array = [];
+
             for (var i = 0; i < collection.geometries.length; ++i) {
                 var geometry = collection.geometries[i];
-                array.push(this.extractGeometry.apply(this, [ geometry ]));
+                array.push(this.extract.geometry.apply(this, [ geometry ]));
             }
+
             return array;
         }
     };
