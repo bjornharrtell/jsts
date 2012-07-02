c4c7229732be6fafbab1997512b1b7eb3548befe
Björn Harrtell <bjorn@wololo.org>
2012-07-02 13:45:20 +0200

    Update build

--- build/build.sh
+++ build/build.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 jsbuild full.cfg -v -o ../lib -j jsts.js
-tar -cvf jsts-0.12.0.tar ../src ../doc ../lib ../examples ../src ../license.txt ../authors.txt ../ChangeLog ../README.md
-rm jsts-0.12.0.tar.gz
-gzip -9 jsts-0.12.0.tar
+tar -cvf jsts-0.12.1.tar ../src ../doc ../lib ../examples ../src ../license.txt ../authors.txt ../ChangeLog ../README.md
+rm jsts-0.12.1.tar.gz
+gzip -9 jsts-0.12.1.tar
 mkdir tmp
 cd tmp
-tar xvfz ../jsts-0.12.0.tar.gz
+tar xvfz ../jsts-0.12.1.tar.gz
 rm ../jsts-0.12.0.zip
-zip -r -9 ../jsts-0.12.0.zip *
+zip -r -9 ../jsts-0.12.1.zip *
 cd ..
 rm tmp -rf
