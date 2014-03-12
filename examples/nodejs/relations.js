b605086cdb8de508165776d232675ccad635b253
erelsgl <erelsgl@gmail.com>
2014-03-12 21:56:52 +0200

    add more relations

--- examples/nodejs/relations.js
+++ examples/nodejs/relations.js
@@ -1,5 +1,5 @@
 /**
- * Example of calculating intersection between a polygon and a line or a point.
+ * Example of calculating intersection between a reference and a line or a point.
  */
 
 var jsts = require("jsts");
@@ -15,12 +15,16 @@ function booleanRelationToString(shape1, shape2, relationName) {
   }
 }
 
-var polygon = reader.read('POLYGON((0 0, 0 10, 10 10, 10 0, 0 0))'); // the square between (0,0) and (10,10)
+var reference = reader.read('POLYGON((0 0, 0 10, 10 10, 10 0, 0 0))'); // the square between (0,0) and (10,10)
 
-var points = {};
-points.internal = reader.read("POINT(7 7)");    // a point inside the square
-points.boundary = reader.read("POINT(10 5)");    // a point on the boundary of the square
-points.external = reader.read("POINT(11 11)");  // a point outside the square
+var others = {};
+others.internalPoint = reader.read("POINT(7 7)");    // a point inside the square
+others.boundaryPoint = reader.read("POINT(10 5)");    // a point on the boundary of the square
+others.externalPoint = reader.read("POINT(11 11)");  // a point outside the square
+others.farawaySquare = reader.read('POLYGON((20 20, 20 30, 30 30, 30 20, 20 20))'); // the square between (20,20) and (30,30)
+others.cornerSquare = reader.read('POLYGON((10 10, 10 30, 30 30, 30 10, 10 10))'); // the square between (10,10) and (30,30)
+others.sideSquare = reader.read('POLYGON((10 0, 10 30, 30 30, 30 0, 10 0))'); // the rectangle between (10,0) and (30,30)
+others.overlappingSquare = reader.read('POLYGON((5 5, 5 20, 20 20, 20 5, 5 5))'); // the square between (5,5) and (20,20)
 
 var relations = [
     "disjoint", "touches", 
@@ -32,13 +36,13 @@ var relations = [
     ];
 
 
-for (var p in points) {
-  console.log("\n"+p+"\n--------\n");
-  var point = points[p];
+for (var p in others) {
+  console.log("\n"+p+"\n-----------------\n");
+  var point = others[p];
   for (var r in relations) {
     var relation = relations[r];
-    console.log("polygon."+relation+"("+p+") = "+polygon[relation](point));
-    console.log(p+"."+relation+"(polygon) = "+point[relation](polygon));
+    console.log("reference."+relation+"("+p+") = "+reference[relation](point));
+    console.log(p+"."+relation+"(reference) = "+point[relation](reference));
   }
 }
 
