540df1afbb018fa431f13dd152b08c33665ab609
erelsgl <erelsgl@gmail.com>
2014-03-13 16:35:14 +0200

    fix 156.js according to documentation

--- test/spec/jsts/issues/156.js
+++ test/spec/jsts/issues/156.js
@@ -2,13 +2,15 @@ describe("GitHub issue #156 - RectangleContains", function() {
   var reader = new jsts.io.WKTReader();
   it("works for two rectangles", function() {
     var a = reader.read('POLYGON((0 0, 0 10, 20 10, 20 0, 0 0))');
-    var external = reader.read('POLYGON((30 30, 30 50, 50 50, 50 30, 30 30))');
-    var boundary = reader.read('POLYGON((0 0, 0 10, 10 10, 10 0, 0 0))');
-    var internal = reader.read('POLYGON((2 2, 2 8, 18 8, 18 2, 2 2))');
-    expect(a.contains(a)).toBe(false);
-    expect(a.contains(external)).toBe(false);
-    expect(a.contains(boundary)).toBe(false);
-    expect(a.contains(internal)).toBe(true);
+    var part_of_a = reader.read('POLYGON((0 0, 0 10, 10 10, 10 0, 0 0))');
+    var inside_a = reader.read('POLYGON((2 2, 2 8, 18 8, 18 2, 2 2))');
+    var boundary_of_a = reader.read('LINESTRING(0 2, 0 8)');
+    var outside_a = reader.read('POLYGON((30 30, 30 50, 50 50, 50 30, 30 30))');
+    expect(a.contains(a)).toBe(true);
+    expect(a.contains(part_of_a)).toBe(true);
+    expect(a.contains(inside_a)).toBe(true);
+    expect(a.contains(boundary_of_a)).toBe(false);
+    expect(a.contains(outside_a)).toBe(false);
   }); 
   it("works for a rectangle and a point", function() {
     var a = reader.read('POLYGON((0 0, 0 10, 20 10, 20 0, 0 0))');
