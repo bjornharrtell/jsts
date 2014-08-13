4880d210d369c7fc13bc456ddd913c99acfc9acd
Björn Harrtell <bjorn@wololo.org>
2014-08-13 23:03:33 +0200

    Fix SpecRunner

--- test/spec/spec_helper.js
+++ test/spec/spec_helper.js
@@ -1,3 +1,6 @@
 // Check if runing with jasmine-node and if so require dependencies into global.
-if (typeof jsts === 'undefined' && typeof require !== 'undefined')
+if (typeof jsts === 'undefined' && typeof require !== 'undefined') {
   global.jsts = require('../..');
+  
+  
+}
