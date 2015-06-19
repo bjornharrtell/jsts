b6a2e4b008b4a2eb0dfb22a60563d43ef3463558
rt <ruantao1989@126.com>
2015-06-19 17:58:41 +0800

    Implement Array.forEach if native not support.
    add the Array.forEach method, for IE6-IE8

--- src/jsts.js
+++ src/jsts.js
@@ -93,6 +93,35 @@ if(typeof String.prototype.trim !== 'function') {
 }
 
 /**
+ * Implement Array.forEach if native support is missing.
+ */
+if (!Array.prototype.forEach) {
+  Array.prototype.forEach = function(callback, thisArg) {
+    var T, k;
+    if (this == null) {
+      throw new TypeError(" this is null or not defined");
+    }
+    var O = Object(this);
+    var len = O.length >>> 0; // Hack to convert O.length to a UInt32  
+    if ({}.toString.call(callback) != "[object Function]") {
+      throw new TypeError(callback + " is not a function");
+    }
+    if (thisArg) {
+      T = thisArg;
+    }
+    k = 0;
+    while (k < len) {
+      var kValue;
+      if (k in O) {
+        kValue = O[k];
+        callback.call(T, kValue, k, O);
+      }
+      k++;
+    }
+  };
+}
+
+/**
  * Global function intended for use as a generic abstract method.
  * @private
  */
