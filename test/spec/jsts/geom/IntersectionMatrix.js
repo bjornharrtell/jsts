86924938c48c7e61c570db48de8f384dea2d5071
chpicone <ch.picone@gmail.com>
2015-07-04 14:02:14 +0200

    Update IntersectionMatrix.js

--- test/spec/jsts/geom/IntersectionMatrix.js
+++ test/spec/jsts/geom/IntersectionMatrix.js
@@ -5,15 +5,16 @@
  */
 
 describe('jsts.geom.IntersectionMatrix', function() {
+  
   it('should be create IntersectionMatrix from string', function() {
     var mat = new jsts.geom.IntersectionMatrix("T*F**F***");
-	var expected = [-2,-3,-1,-3,-3,-1,-3,-3,-3];
-	expect(mat.matrix.length).toBe(3);
-	for(var r=0; r<3; r++){
-		for(var c=0; c<3; c++){
-			expect(mat.get(r,c)).toBe(expected[r*3+c]);
-		}
-	}
+    var expected = [-2,-3,-1,-3,-3,-1,-3,-3,-3];
+    expect(mat.matrix.length).toBe(3);
+    for(var r=0; r<3; r++){
+      for(var c=0; c<3; c++){
+        expect(mat.get(r,c)).toBe(expected[r*3+c]);
+      }
+    }
   });
   
 });
