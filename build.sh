f9b8742c9382d3a4dbfd202dd78e3b67d935ec2f
Mike Atlas <mikeatlas@gmail.com>
2015-08-26 22:32:05 -0400

    clean the lib/dist folders and don't fail if they already exist

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-rm dist -rf
-mkdir lib
+rm lib -rf
+mkdir -p lib
 jsbuild build/full.cfg -v -o lib -j jsts.min.js
 jsbuild build/full.cfg -u -v -o lib -j jsts.js
 rm dist -rf
-mkdir dist
+mkdir -p dist
 tar -zcvf dist/jsts-0.17.0.tar.gz src lib examples src license.txt authors.txt ChangeLog README.md
 zip -r -9 dist/jsts-0.17.0.zip src lib examples src license.txt authors.txt ChangeLog README.md
 
