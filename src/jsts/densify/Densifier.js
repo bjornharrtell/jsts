29e5fb5df33d2c67e1933928b731b914a0ce2818
Gregers Petersen <gregers@septima.dk>
2015-10-19 15:26:32 +0200

    Added first shot on jsts.densify.Densifier

--- src/jsts/densify/Densifier.js
+++ src/jsts/densify/Densifier.js
@@ -4,6 +4,8 @@
  * See /license.txt for the full text of the license.
 */
 
+
+(function() {
 /**
  * Densifies a {@link Geometry} by inserting extra vertices along the line
  * segments contained in the geometry.  All segments in the created densified
@@ -12,7 +14,12 @@
  * The coordinates created during densification respect the input geometry's
  * {@link PrecisionModel}.
  *
- * @author Stephan Telling
+ * @author Stephan Telling, Septima.dk
+ * @author (Hans) Gregers Petersen, Septima.dk
+ */
+
+
+/*
  *
  * @constructor
  */
@@ -21,20 +28,21 @@ jsts.densify.Densifier = function(geom) {
 };
 
 jsts.densify.Densifier.prototype.densify = function(geom, distanceTolerance) {
-  densifier = new jsts.densify.Densifier(geom);
+  var densifier = new jsts.densify.Densifier(geom);
   densifier.setDistanceTolerance(distanceTolerance);
   return densifier.getResultGeometry();
 };
 
-jsts.densify.Densifier.prototype.densifyPoints = function(pts, distanceTolerance, precModel) {
-  var lineSegment = new jsts.geom.LineSegment();
+// Static function, so no prototyping...
+jsts.densify.Densifier.densifyPoints = function(pts, distanceTolerance, precModel) {
+  var seg = new jsts.geom.LineSegment();
   var coordinateList = new jsts.geom.CoordinateList();
 
   for (i = 0; i < pts.length - 1; i++) {
-    lineSegment.p0 = pts[i];
-    lineSegment.p1 = pts[i + 1];
-    coordinateList.add(lineSegment.p0, false);
-    var len = lineSegment.getLength();
+    seg.p0 = pts[i];
+    seg.p1 = pts[i + 1];
+    coordinateList.add(seg.p0, false);
+    var len = seg.getLength();
     var densifiedSegCount = (len / distanceTolerance) + 1;
 
     if (densifiedSegCount > 1) {
@@ -42,7 +50,7 @@ jsts.densify.Densifier.prototype.densifyPoints = function(pts, distanceTolerance
       for (j = 1; j < densifiedSegCount; j++) {
         var segFract = (j * densifiedSegLen) / len;
         var p = seg.pointAlong(segFract);
-        p = precModel.makePrecise(p);
+        precModel.makePrecise(p);
         coordinateList.add(p, false);
       }
     }
@@ -50,6 +58,7 @@ jsts.densify.Densifier.prototype.densifyPoints = function(pts, distanceTolerance
   coordinateList.add(pts[pts.length - 1], false);
   return coordinateList.toCoordinateArray();
 };
+
 
 jsts.densify.Densifier.prototype.setDistanceTolerance = function(distanceTolerance) {
   if (distanceTolerance <= 0.0) {
@@ -59,33 +68,45 @@ jsts.densify.Densifier.prototype.setDistanceTolerance = function(distanceToleran
   this.distanceTolerance = distanceTolerance;
 };
 
+
 jsts.densify.Densifier.prototype.getResultGeometry = function() {
   return (new jsts.densify.Densifier.DensifyTransformer()).transform(this.geom);
 };
+
+
+
 
 /*
  * Class DensifyTransformer from http://sourceforge.net/p/jts-topo-suite/code/HEAD/tree/trunk/jts/java/src/com/vividsolutions/jts/densify/Densifier.java
  */
 
-jsts.densify.Densifier.DensifyTransformer = function() {};
+// Extend jsts.geom.util.GeometryTransformer by calling constructor and prototyping new object
+jsts.densify.Densifier.DensifyTransformer = function() {
+	jsts.geom.util.GeometryTransformer.call(this); // super called
+};
 
 jsts.densify.Densifier.DensifyTransformer.prototype = new jsts.geom.util.GeometryTransformer();
-//jsts.densify.Densifier.DensifyTransformer.constructor = jsts.densify.Densifier.DensifyTransformer;
+jsts.densify.Densifier.DensifyTransformer.constructor = jsts.densify.Densifier.DensifyTransformer;
+jsts.densify.Densifier.DensifyTransformer.CLASS_NAME = 'jsts.densify.Densifier.DensifyTransformer';
 
-// Below not added to prototype, as they're protected in the java source
 jsts.densify.Densifier.DensifyTransformer.prototype.transformCoordinates = function(coords, parent) {
-  inputPts = coords.toCoordinateArray();
-  newPts = jsts.densify.Densifier.densifyPoints(inputPts, this.distanceTolerance, parent.getPrecisionModel());
+  var inputPts = coords;
+  if( !(inputPts instanceof Array) ) {  inputPts = coords.toCoordinateArray() };
+
+  this.distanceTolerance = 0.5; //TODO: It was a nested class, so it had access to distanceTolerance, but not in JS :-|
+  var newPts = jsts.densify.Densifier.densifyPoints(inputPts, this.distanceTolerance, parent.getPrecisionModel());
 
   if (parent instanceof jsts.geom.LineString && newPts.length == 1) {
     newPts = new jsts.geom.Coordinate(0);
   }
 
-  return this.factory.getCoordinateSequenceFactory().create(newPts);
+ // Would like to do
+ //  return this.factory.getCoordinateSequenceFactory().create(newPts);
+  return newPts;
 };
 
 jsts.densify.Densifier.DensifyTransformer.prototype.transformPolygon = function(geom, parent) {
-  roughGeom = this.transformPolygon(geom, parent);
+   var roughGeom = jsts.geom.util.GeometryTransformer.prototype.transformPolygon.call(this, geom, parent); // Special super call :-|
 
   if (parent instanceof jsts.geom.MultiPolygon) {
     return roughGeom;
@@ -94,10 +115,14 @@ jsts.densify.Densifier.DensifyTransformer.prototype.transformPolygon = function(
 };
 
 jsts.densify.Densifier.DensifyTransformer.prototype.transformMultiPolygon = function(geom, parent) {
-  roughGeom = this.transformMultiPolygon(geom, parent);
+  var roughGeom = this._super.transformMultiPolygon(geom, parent);
   return this.createValidArea(roughGeom);
 };
 
 jsts.densify.Densifier.DensifyTransformer.prototype.createValidArea = function(roughAreaGeom) {
   return roughAreaGeom.buffer(0.0);
 };
+
+jsts.densify.Densifier.DensifyTransformer.CLASS_NAME = 'jsts.densify.Densifier.DensifyTransformer';
+
+})();
