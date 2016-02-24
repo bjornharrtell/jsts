482f3d7c5c9b608490c3ee276582069ec716e311
Björn Harrtell <bjorn@wololo.org>
2016-02-24 13:41:16 +0100

    Add specific tests for MultiPoint (#232)

---
+++ test/spec/issues/232.js
@@ -0,0 +1,21 @@
+import expect from 'expect.js'
+
+import Envelope from 'org/locationtech/jts/geom/Envelope'
+import WKTReader from 'org/locationtech/jts/io/WKTReader'
+
+describe('MultiPoint does not support getEnvelopeInternal (#232)', function () {
+  var reader = new WKTReader()
+
+  it('MultiPoint getEnvelopeInternal should work', function () {
+    const mp = reader.read('MULTIPOINT ((10 10), (10 20), (20 20))')
+    const result = mp.getEnvelopeInternal()
+    const expected = new Envelope(10, 20, 10, 20)
+    expect(result.equals(expected)).to.be(true)
+  })
+
+  it('Evaluating if MultiPoint covers Polygon should work', function () {
+    const p = reader.read('POLYGON((10 10, 100 10, 100 100, 10 100, 10 10))')
+    const mp = reader.read('MULTIPOINT ((10 10), (10 20), (20 20))')
+    expect(p.covers(mp)).to.be(true)
+  })
+})
