f5f82681a9b768822e3d6a21db31f6119b1e88a3
Björn Harrtell <bjorn@wololo.org>
2016-02-25 07:55:08 +0100

    Tests housekeeping

--- test/spec/issues/232.js
+++ test/spec/issues/232.js
@@ -6,14 +6,14 @@ import WKTReader from 'org/locationtech/jts/io/WKTReader'
 describe('MultiPoint does not support getEnvelopeInternal (#232)', function () {
   var reader = new WKTReader()
 
-  it('MultiPoint getEnvelopeInternal should work', function () {
+  it('getEnvelopeInternal should work', function () {
     const mp = reader.read('MULTIPOINT ((10 10), (10 20), (20 20))')
     const result = mp.getEnvelopeInternal()
     const expected = new Envelope(10, 20, 10, 20)
     expect(result.equals(expected)).to.be(true)
   })
 
-  it('Evaluating if MultiPoint covers Polygon should work', function () {
+  it('test for covering Polygon should work', function () {
     const p = reader.read('POLYGON((10 10, 100 10, 100 100, 10 100, 10 10))')
     const mp = reader.read('MULTIPOINT ((10 10), (10 20), (20 20))')
     expect(p.covers(mp)).to.be(true)
---
+++ test/spec/issues/236.js
@@ -0,0 +1,13 @@
+import expect from 'expect.js'
+
+import WKTReader from 'org/locationtech/jts/io/WKTReader'
+
+describe('IsValid throws exception in some cases (#236)', function () {
+  var reader = new WKTReader()
+
+  it('isValid should not throw an exception for this Polygon', function () {
+    const p = reader.read('POLYGON((460901 -33906,3961797 -2530706,2293179 -4841460,599984 -3210077,460901 -33906))')
+    const valid = p.isValid()
+    expect(valid).to.be(true)
+  })
+})
