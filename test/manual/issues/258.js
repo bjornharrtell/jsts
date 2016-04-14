6dc96aa605bddaee6fc8b7f428d0c76450de8e17
Björn Harrtell <bjorn@wololo.org>
2016-04-14 09:29:17 +0200

    Add test case for #258

---
+++ test/manual/issues/258.js
@@ -0,0 +1,24 @@
+import expect from 'expect.js'
+
+import Coordinate from 'org/locationtech/jts/geom/Coordinate'
+import GeometryFactory from 'org/locationtech/jts/geom/GeometryFactory'
+import OverlayOp from 'org/locationtech/jts/operation/overlay/OverlayOp'
+
+describe('GeometryComponentFilter is not defined on polygon intersection (#258)', function () {
+  const factory = new GeometryFactory()
+
+  it('Intersection small polygons should not cause Error', function () {
+    const coordinates = [
+      new Coordinate(50.3282361844247, 43.051352042895246),
+      new Coordinate(50.3286311088063, 43.05588339079827),
+      new Coordinate(50.3289594043318, 43.05119738555413),
+      new Coordinate(50.32935432871337, 43.0557287321717),
+      new Coordinate(50.3282361844247, 43.051352042895246)
+    ]
+
+    const polygon1 = factory.createPolygon(factory.createLinearRing(coordinates))
+    const polygon2 = factory.createPolygon(factory.createLinearRing(coordinates))
+
+    OverlayOp.intersection(polygon1, polygon2)
+  })
+})
