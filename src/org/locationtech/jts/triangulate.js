9ea1dbe5aef7e29fc9ab1a030ca07beed02073f5
Björn Harrtell <bjorn@wololo.org>
2016-09-14 09:23:20 +0200

    Export quadedge/Vertex (#286)

--- src/org/locationtech/jts/triangulate.js
+++ src/org/locationtech/jts/triangulate.js
@@ -1,9 +1,11 @@
 import ConformingDelaunayTriangulationBuilder from './triangulate/ConformingDelaunayTriangulationBuilder'
 import DelaunayTriangulationBuilder from './triangulate/DelaunayTriangulationBuilder'
 import VoronoiDiagramBuilder from './triangulate/VoronoiDiagramBuilder'
+import * as quadedge from './triangulate/quadedge'
 
 export {
   ConformingDelaunayTriangulationBuilder,
   DelaunayTriangulationBuilder,
-  VoronoiDiagramBuilder
+  VoronoiDiagramBuilder,
+  quadedge
 }
---
+++ src/org/locationtech/jts/triangulate/quadedge.js
@@ -0,0 +1,5 @@
+import Vertex from './quadedge/Vertex'
+
+export {
+  Vertex
+}
