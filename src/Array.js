ddc08c9d0daa77b23b52396f78467cb547e56ee8
Thomas Chandelle <thomas.chandelle@1spatial.com>
2016-10-21 09:24:29 +0200

    Implement a better polyfill for Array#fill
    The current polyfill pollute the Array prototype, fill method is
    listable with for in loop.

--- src/Array.js
+++ src/Array.js
@@ -1,22 +1,36 @@
-if (!Array.prototype.fill) {
-  Array.prototype.fill = function (value) { // eslint-disable-line
-    var O = Object(this)
-    var len = parseInt(O.length, 10)
-    var start = arguments[1]
-    var relativeStart = parseInt(start, 10) || 0
-    var k = relativeStart < 0
-      ? Math.max(len + relativeStart, 0)
-      : Math.min(relativeStart, len)
-    var end = arguments[2]
-    var relativeEnd = end === undefined
-      ? len
-      : (parseInt(end, 10) || 0)
-    var final = relativeEnd < 0
-      ? Math.max(len + relativeEnd, 0)
-      : Math.min(relativeEnd, len)
-    for (; k < final; k++) {
-      O[k] = value
-    }
-    return O
-  }
+/* Polyfill service v3.13.0
+ * For detailed credits and licence information see http://github.com/financial-times/polyfill-service
+ * 
+ * - Array.prototype.fill, License: CC0 */
+
+if (!('fill' in Array.prototype)) {
+  Object.defineProperty(Array.prototype, 'fill', {
+    configurable: true,
+    value: function fill(value) {
+      if (this === undefined || this === null) {
+        throw new TypeError(this + ' is not an object');
+      }
+
+      var arrayLike = Object(this);
+
+      var length = Math.max(Math.min(arrayLike.length, 9007199254740991), 0) || 0;
+
+      var relativeStart = 1 in arguments ? parseInt(Number(arguments[1]), 10) || 0 : 0;
+
+      relativeStart = relativeStart < 0 ? Math.max(length + relativeStart, 0) : Math.min(relativeStart, length);
+
+      var relativeEnd = 2 in arguments && arguments[2] !== undefined ? parseInt(Number(arguments[2]), 10) || 0 : length;
+
+      relativeEnd = relativeEnd < 0 ? Math.max(length + arguments[2], 0) : Math.min(relativeEnd, length);
+
+      while (relativeStart < relativeEnd) {
+        arrayLike[relativeStart] = value;
+
+        ++relativeStart;
+      }
+
+      return arrayLike;
+    },
+    writable: true
+  });
 }
