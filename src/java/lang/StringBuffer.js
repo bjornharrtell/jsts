d5364efec919ba933faa2868fb34e8bf2d364682
Björn Harrtell <bjorn@wololo.org>
2016-11-04 21:42:32 +0100

    Fix StringBuffer setCharAt to mutate internal string as expected

--- src/java/lang/StringBuffer.js
+++ src/java/lang/StringBuffer.js
@@ -7,7 +7,7 @@ StringBuffer.prototype.append = function (e) {
 }
 
 StringBuffer.prototype.setCharAt = function (i, c) {
-  return this.str.substr(0, i) + c + this.str.substr(i + 1)
+  this.str = this.str.substr(0, i) + c + this.str.substr(i + 1)
 }
 
 StringBuffer.prototype.toString = function (e) {
