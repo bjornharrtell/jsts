b00d33e736c8d64483f664793647ddb0115cc43f
Björn Harrtell <bjorn@wololo.org>
2016-12-30 10:34:51 +0100

    Generate source map (#267)

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,22 @@
-var pjson = require('./package.json')
+import fs from 'fs'
 import git from 'git-rev-sync'
 import replace from 'rollup-plugin-replace'
 // import nodeResolve from 'rollup-plugin-node-resolve'
 // import commonjs from 'rollup-plugin-commonjs'
 import babel from 'rollup-plugin-babel'
 
+const packageJson = JSON.parse(fs.readFileSync('./package.json'))
+const license = fs.readFileSync('./license.txt')
+
 export default {
   entry: 'src/jsts.js',
   format: 'umd',
   moduleName: 'jsts',
+  banner: license,
+  sourceMap: true,
   plugins: [
     replace({
-      npm_package_version: pjson.version,
+      npm_package_version: packageJson.version,
       git_hash: git.short()
     }),
     /*
