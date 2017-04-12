93f117d64666dd0fa5b3014359e8b8060120ef2c
Björn Harrtell <bjorn@wololo.org>
2017-04-12 18:27:27 +0200

    Export public classes from linearref

---
+++ src/org/locationtech/jts/linearref.js
@@ -0,0 +1,15 @@
+import LengthIndexedLine from './linearref/LengthIndexedLine'
+import LengthLocationMap from './linearref/LengthLocationMap'
+import LinearGeometryBuilder from './linearref/LinearGeometryBuilder'
+import LinearIterator from './linearref/LinearIterator'
+import LinearLocation from './linearref/LinearLocation'
+import LocationIndexedLine from './linearref/LocationIndexedLine'
+
+export {
+  LengthIndexedLine,
+  LengthLocationMap,
+  LinearGeometryBuilder,
+  LinearIterator,
+  LinearLocation,
+  LocationIndexedLine
+}
