3b832e54aaed4e365d9a47066ae68af17d099b0a
Denis <carriere.denis@gmail.com>
2017-09-24 20:47:05 -0400

    Modify Rollup configurations

--- rollup.config.js
+++ rollup.config.js
@@ -2,33 +2,58 @@ import fs from 'fs'
 import git from 'git-rev-sync'
 import replace from 'rollup-plugin-replace'
 import babel from 'rollup-plugin-babel'
+import uglify from 'rollup-plugin-uglify'
 
-const packageJson = JSON.parse(fs.readFileSync('./package.json'))
+const pckg = require('./package.json')
 const license = fs.readFileSync('./license.txt')
 
-export default {
-  entry: 'src/jsts.js',
-  format: 'umd',
-  moduleName: 'jsts',
-  banner: license,
-  sourceMap: true,
-  plugins: [
-    replace({
-      npm_package_version: packageJson.version,
-      git_hash: git.short()
-    }),
-    babel({
-      exclude: 'node_modules/**',
-      presets: [['env', {
-        modules: false,
-        targets: {
-          browsers: ['last 2 versions', 'ie >= 11']
-        }
-      }]],
-      plugins: [
-        'external-helpers'
-      ],
-      babelrc: false
-    })
-  ]
+/**
+ * Babel Rollup Plugins
+ */
+const babelPlugins = [
+  replace({
+    npm_package_version: pckg.version,
+    git_hash: git.short()
+  }),
+  babel({
+    exclude: 'node_modules/**',
+    presets: [['env', {
+      modules: false,
+      targets: {
+        browsers: ['last 2 versions', 'ie >= 11']
+      }
+    }]],
+    plugins: [
+      'external-helpers'
+    ],
+    babelrc: false
+  })
+]
+
+/**
+ * Minify Rollup Plugins
+ */
+const minifyPlugins = [uglify(), ...babelPlugins]
+
+/**
+ * Assign default Rollup configs
+ */
+function assign({file, format, plugins}) {
+  return {
+    input: 'src/jsts.js',
+    output: {
+      file,
+      format: format || 'umd',
+      name: 'jsts',
+      banner: license,
+      sourcemap: true,
+    },
+    plugins: [...plugins || []]
+  }
 }
+
+export default [
+  assign({ format: 'cjs', file: 'dist/jsts.js' }),
+  assign({ format: 'es', file: 'dist/jsts.mjs' }),
+  assign({ format: 'umd', file: 'dist/jsts.min.js', plugins: minifyPlugins })
+]
