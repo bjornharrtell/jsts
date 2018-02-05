698f522d6e493bc3274c344c91127ba0b775160d
Björn Harrtell <bjorn@wololo.org>
2018-02-05 20:05:45 +0100

    Add kdtree to exports

--- src/org/locationtech/jts/index.js
+++ src/org/locationtech/jts/index.js
@@ -1,4 +1,5 @@
+import * as kdtree from './index/kdtree'
 import * as quadtree from './index/quadtree'
 import * as strtree from './index/strtree'
 
-export { quadtree, strtree }
+export { kdtree, quadtree, strtree }
---
+++ src/org/locationtech/jts/index/kdtree.js
@@ -0,0 +1,5 @@
+import KdTree from './kdtree/KdTree'
+
+export {
+  KdTree
+}
