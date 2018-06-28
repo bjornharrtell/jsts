002d9c03a68bb96caa666760e830efff2fd270df
Björn Harrtell <bjorn@septima.dk>
2018-06-28 11:04:18 +0200

    Add polyfill for Math.log2

--- src/Math.js
+++ src/Math.js
@@ -1,3 +1,7 @@
 Math.trunc = Math.trunc || function(x) {
-  return x < 0 ? Math.ceil(x) : Math.floor(x);
+  return x < 0 ? Math.ceil(x) : Math.floor(x)
+}
+
+Math.log2 = Math.log2 || function(x) {
+  return Math.log(x) * Math.LOG2E
 }
