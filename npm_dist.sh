c12b9dcaa610bf2f1bfd1155b7b96f7041065191
Björn Harrtell <bjorn@wololo.org>
2018-07-15 22:08:55 +0200

    Fixup npm_dist

--- npm_dist.sh
+++ npm_dist.sh
@@ -5,7 +5,7 @@ mkdir ./dist/dist
 mv ./dist/jsts.* ./dist/dist
 cp -r ./src/java ./dist/java
 cp -r ./src/org ./dist/org
-cp ./src/index.js ./dist/index.js
+cp ./index.js ./dist/index.js
 cp ./src/inherits.js ./dist/inherits.js
 cp ./src/hasInterface.js ./dist/hasInterface.js
 cp ./src/extend.js ./dist/extend.js
