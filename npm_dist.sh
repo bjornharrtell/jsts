b7a330f52f5d44c3cbc1594788b5b5da7ff9bbaf
Björn Harrtell <bjorn@wololo.org>
2018-09-15 12:44:38 +0200

    Minor post release updates

--- npm_dist.sh
+++ npm_dist.sh
@@ -5,10 +5,7 @@ mkdir ./dist/dist
 mv ./dist/jsts.* ./dist/dist
 cp -r ./src/java ./dist/java
 cp -r ./src/org ./dist/org
-cp ./index.js ./dist/index.js
-cp ./src/inherits.js ./dist/inherits.js
 cp ./src/hasInterface.js ./dist/hasInterface.js
-cp ./src/extend.js ./dist/extend.js
 cp ./src/Map.js ./dist/Map.js
 cp ./package.json ./dist/package.json
 cp ./README.md ./dist/README.md
