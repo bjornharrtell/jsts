c01c099c5d74977fc9cc39070989a2440076c34b
Björn Harrtell <bjorn@wololo.org>
2018-09-15 12:35:06 +0200

    Fix for non-argument super call

--- test/manual/operation/polygonize/Polygonizer.js
+++ test/manual/operation/polygonize/Polygonizer.js
@@ -8,7 +8,7 @@ import Polygonizer from 'org/locationtech/jts/operation/polygonize/Polygonizer'
 describe('Polygonizer', function () {
   it('Basic Polygonizer test', function () {
     let reader = new WKTReader();
-    let lineString = reader.read('LINESTRING (30 10, 10 30, 40 40)');
+    let lineString = reader.read('LINESTRING (30 10, 40 40, 20 40, 10 20, 30 10)');
     
     var polygonizer = new Polygonizer();
     expect(polygonizer).to.be.ok();
