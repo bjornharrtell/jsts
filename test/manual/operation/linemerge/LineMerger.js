e4f1f41563d413f9255b9e077eec912a6bb39608
Björn Harrtell <bjornharrtell@users.noreply.github.com>
2019-02-03 13:17:26 +0100

    Fix LineMerger (#374)

---
+++ test/manual/operation/linemerge/LineMerger.js
@@ -0,0 +1,23 @@
+import expect from 'expect.js'
+
+import WKTReader from 'org/locationtech/jts/io/WKTReader'
+import WKTWriter from 'org/locationtech/jts/io/WKTWriter'
+import LineMerger from 'org/locationtech/jts/operation/linemerge/LineMerger'
+
+describe('LineMerger', function () {
+  it('#373', function () {
+    const reader = new WKTReader()
+    const ls1 = reader.read('LINESTRING(0 0, 1 1)')
+    const ls2 = reader.read('LINESTRING(1 1, 2 2)')
+    const lineMerger = new LineMerger()
+    lineMerger.add(ls1)
+    lineMerger.add(ls2)
+    lineMerger.merge()
+    const mergedLineStrings = lineMerger.getMergedLineStrings()
+    const mergedLineString = mergedLineStrings.get(0)
+    const writer = new WKTWriter()
+    const result = writer.write(mergedLineString)
+    console.log(result)
+    expect(result).to.equal('LINESTRING(0 0,1 1,2 2)')
+  })
+})
