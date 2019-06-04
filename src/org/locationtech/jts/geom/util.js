cdcd00f99e2b2b98759e13ab8b64c4eca31ffc33
William Wall <wwall@caci.com>
2019-06-04 15:48:41 -0600

    Add util packages to single file build
    resolves #379

---
+++ src/org/locationtech/jts/geom/util.js
@@ -0,0 +1,39 @@
+import AffineTransformation from './util/AffineTransformation'
+import AffineTransformationBuilder from './util/AffineTransformationBuilder'
+import AffineTransformationFactory from './util/AffineTransformationFactory'
+import ComponentCoordinateExtracter from './util/ComponentCoordinateExtracter'
+import GeometryCollectionMapper from './util/GeometryCollectionMapper'
+import GeometryCombiner from './util/GeometryCombiner'
+import GeometryEditor from './util/GeometryEditor'
+import GeometryExtracter from './util/GeometryExtracter'
+import GeometryMapper from './util/GeometryMapper'
+import GeometryTransformer from './util/GeometryTransformer'
+import LineStringExtracter from './util/LineStringExtracter'
+import LinearComponentExtracter from './util/LinearComponentExtracter'
+import MapOp from './util/MapOp'
+import NoninvertibleTransformationException from './util/NoninvertibleTransformationException'
+import PointExtracter from './util/PointExtracter'
+import PolygonExtracter from './util/PolygonExtracter'
+import ShortCircuitedGeometryVisitor from './util/ShortCircuitedGeometryVisitor'
+import SineStarFactory from './util/SineStarFactory'
+
+export {
+  AffineTransformation,
+  AffineTransformationBuilder,
+  AffineTransformationFactory,
+  ComponentCoordinateExtracter,
+  GeometryCollectionMapper,
+  GeometryCombiner,
+  GeometryEditor,
+  GeometryExtracter,
+  GeometryMapper,
+  GeometryTransformer,
+  LineStringExtracter,
+  LinearComponentExtracter,
+  MapOp,
+  NoninvertibleTransformationException,
+  PointExtracter,
+  PolygonExtracter,
+  ShortCircuitedGeometryVisitor,
+  SineStarFactory
+}
---
+++ src/org/locationtech/jts/util.js
@@ -0,0 +1,27 @@
+import Assert from './util/Assert'
+import AssertionFailedException from './util/AssertionFailedException'
+import CollectionUtil from './util/CollectionUtil'
+import CoordinateArrayFilter from './util/CoordinateArrayFilter'
+import CoordinateCountFilter from './util/CoordinateCountFilter'
+import GeometricShapeFactory from './util/GeometricShapeFactory'
+import Memory from './util/Memory'
+import NumberUtil from './util/NumberUtil'
+import ObjectCounter from './util/ObjectCounter'
+import PriorityQueue from './util/PriorityQueue'
+import StringUtil from './util/StringUtil'
+import UniqueCoordinateArrayFilter from './util/UniqueCoordinateArrayFilter'
+
+export {
+  Assert,
+  AssertionFailedException,
+  CollectionUtil,
+  CoordinateArrayFilter,
+  CoordinateCountFilter,
+  GeometricShapeFactory,
+  Memory,
+  NumberUtil,
+  ObjectCounter,
+  PriorityQueue,
+  StringUtil,
+  UniqueCoordinateArrayFilter
+}
