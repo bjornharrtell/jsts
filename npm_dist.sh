90a523ec673fb45f62b2c852812af2aecd3548aa
Björn Harrtell <bjorn@septima.dk>
2019-07-13 16:26:59 +0200

    Remaining adjustments to 2.0.5

--- npm_dist.sh
+++ npm_dist.sh
@@ -6,6 +6,5 @@ mv ./dist/jsts.* ./dist/dist
 cp -r ./src/java ./dist/java
 cp -r ./src/org ./dist/org
 cp ./src/hasInterface.js ./dist/hasInterface.js
-cp ./src/Map.js ./dist/Map.js
 cp ./package.json ./dist/package.json
 cp ./README.md ./dist/README.md
