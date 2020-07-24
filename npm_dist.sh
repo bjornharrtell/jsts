0afbe218e44d72c78ab933dd34a05704f23e505e
Björn Harrtell <bjorn@wololo.org>
2020-07-24 13:37:44 +0200

    Fix removal of ast files

--- npm_dist.sh
+++ npm_dist.sh
@@ -3,7 +3,7 @@
 #mkdir ./dist
 mkdir ./dist/dist
 mv ./dist/jsts.* ./dist/dist
-rm ./dist/**/*.ast
+find ./dist -name '*.ast' -type f -delete
 cp -r ./src/java ./dist/java
 cp -r ./src/org ./dist/org
 cp ./src/hasInterface.js ./dist/hasInterface.js
