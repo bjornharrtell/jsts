7f356da09d519be8a5f7d944a61830348be1f88b
Björn Harrtell <bjorn@wololo.org>
2020-10-23 13:53:39 +0200

    Move to right place

--- npm_dist.sh
+++ npm_dist.sh
@@ -3,9 +3,9 @@
 #mkdir ./dist
 mkdir ./dist/dist
 mv ./dist/jsts.* ./dist/dist
-find ./dist -name '*.ast' -type f -delete
 cp -r ./src/java ./dist/java
 cp -r ./src/org ./dist/org
 cp ./src/hasInterface.js ./dist/hasInterface.js
 cp ./package.json ./dist/package.json
 cp ./README.md ./dist/README.md
+find ./dist -name '*.ast' -type f -delete
