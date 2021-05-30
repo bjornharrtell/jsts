444ffc0f0d3c2eb75b6659f2d9afc93779f1d3e1
Gureev Eugene <35802704+iGureEV@users.noreply.github.com>
2021-05-31 02:51:19 +0300

    Update precision.js
    Added the MinimumClearance class to the build

--- src/org/locationtech/jts/precision.js
+++ src/org/locationtech/jts/precision.js
@@ -3,11 +3,13 @@ import CommonBitsOp from './precision/CommonBitsOp'
 import CommonBitsRemover from './precision/CommonBitsRemover'
 import EnhancedPrecisionOp from './precision/EnhancedPrecisionOp'
 import GeometryPrecisionReducer from './precision/GeometryPrecisionReducer'
+import MinimumClearance from './precision/MinimumClearance'
 
 export {
   CommonBits,
   CommonBitsOp,
   CommonBitsRemover,
   EnhancedPrecisionOp,
-  GeometryPrecisionReducer
+  GeometryPrecisionReducer,
+  MinimumClearance
 }
