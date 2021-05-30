def411d485413128fd03241b58dbff970b5ceade
Gureev Eugene <35802704+iGureEV@users.noreply.github.com>
2021-05-30 23:32:20 +0300

    Update precision.js
    For use as a single-file library, the following classes are added to the assembly: CommonBits, CommonBitsOp, CommonBitsRemover, and EnhancedPrecisionOp

--- src/org/locationtech/jts/precision.js
+++ src/org/locationtech/jts/precision.js
@@ -1,5 +1,13 @@
+import CommonBits from './precision/CommonBits'
+import CommonBitsOp from './precision/CommonBitsOp'
+import CommonBitsRemover from './precision/CommonBitsRemover'
+import EnhancedPrecisionOp from './precision/EnhancedPrecisionOp'
 import GeometryPrecisionReducer from './precision/GeometryPrecisionReducer'
 
 export {
+  CommonBits,
+  CommonBitsOp,
+  CommonBitsRemover,
+  EnhancedPrecisionOp,
   GeometryPrecisionReducer
 }
