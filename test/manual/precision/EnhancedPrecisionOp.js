528a3259588d69b68d00bbe38319f949cf9845e7
Gureev Eugene <35802704+iGureEV@users.noreply.github.com>
2021-05-31 00:30:02 +0300

    Update EnhancedPrecisionOp.js
    Added tests for all the functions of the EnhancedPrecisionOp module: union, intersection, buffer, symDifference, difference

--- test/manual/precision/EnhancedPrecisionOp.js
+++ test/manual/precision/EnhancedPrecisionOp.js
@@ -10,20 +10,90 @@ describe('EnhancedPrecisionOp', function() {
     var wkt2 = "POLYGON ((708258.754920656 2402197.91172757, 708257.029447455 2402206.56901508, 708652.961095455 2402312.65463437, 708657.068786251 2402304.6356364, 708258.754920656 2402197.91172757))";
     var g1 = reader.read(wkt1);
     var g2 = reader.read(wkt2);
+	var buf = 10;
 
+	describe('union', function() {
+		it('Cannot be done with simple union', function() {
+			var wasException = false;
+			try {
+				g1.union(g2);
+			} catch (ex) {
+				wasException = true;
+			}
+			expect(wasException).to.be.true;
+		});
 
-    it('Cannot be done with simple intersection', function() {
-        var wasException = false;
-        try {
-            g1.intersection(g2);
-        } catch (ex) {
-            wasException = true;
-        }
-        expect(wasException).to.be.true;
-    });
+		it('Computed with EnhancedPrecisionOp', function() {
+			var res = EnhancedPrecisionOp.union(g1, g2);
+			expect(res).to.be.defined;
+		});
+	});
 
-    it('Computed with EnhancedPrecisionOp', function() {
-        var res = EnhancedPrecisionOp.intersection(g1, g2);
-        expect(res).to.be.defined;
-    });
+	describe('intersection', function() {
+		it('Cannot be done with simple intersection', function() {
+			var wasException = false;
+			try {
+				g1.intersection(g2);
+			} catch (ex) {
+				wasException = true;
+			}
+			expect(wasException).to.be.true;
+		});
+
+		it('Computed with EnhancedPrecisionOp', function() {
+			var res = EnhancedPrecisionOp.intersection(g1, g2);
+			expect(res).to.be.defined;
+		});
+	});
+
+	describe('buffer', function() {
+		it('Cannot be done with simple buffer', function() {
+			var wasException = false;
+			try {
+				g1.buffer(buf);
+			} catch (ex) {
+				wasException = true;
+			}
+			expect(wasException).to.be.true;
+		});
+
+		it('Computed with EnhancedPrecisionOp', function() {
+			var res = EnhancedPrecisionOp.buffer(g1, buf);
+			expect(res).to.be.defined;
+		});
+	});
+
+	describe('symDifference', function() {
+		it('Cannot be done with simple symDifference', function() {
+			var wasException = false;
+			try {
+				g1.union(g2);
+			} catch (ex) {
+				wasException = true;
+			}
+			expect(wasException).to.be.true;
+		});
+
+		it('Computed with EnhancedPrecisionOp', function() {
+			var res = EnhancedPrecisionOp.symDifference(g1, g2);
+			expect(res).to.be.defined;
+		});
+	});
+
+	describe('difference', function() {
+		it('Cannot be done with simple difference', function() {
+			var wasException = false;
+			try {
+				g1.difference(g2);
+			} catch (ex) {
+				wasException = true;
+			}
+			expect(wasException).to.be.true;
+		});
+
+		it('Computed with EnhancedPrecisionOp', function() {
+			var res = EnhancedPrecisionOp.difference(g1, g2);
+			expect(res).to.be.defined;
+		});
+	});
 });
